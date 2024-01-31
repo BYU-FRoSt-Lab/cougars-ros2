f7aa664130f274d63b484ac31552dec6f34bc99a
Nelson Durrant <snelsondurrant@gmail.com>
2024-01-31 23:37:09 +0000

    Changes for PCB testing

--- sync.sh
+++ sync.sh
@@ -3,3 +3,4 @@
 # syncs changes made in the ros2 frost_interfaces package with the teensy_ws
 rsync -avu --delete ~/ros2_ws/src/frost_interfaces ~/teensy_ws/sensors/extra_packages
 rsync -avu --delete ~/ros2_ws/src/frost_interfaces ~/teensy_ws/control/extra_packages
+rsync -avu --delete ~/ros2_ws/src/frost_interfaces ~/teensy_ws/test_ros/extra_packages
