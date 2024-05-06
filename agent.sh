3de3b259ed8d8c5613b14a072433fcc17de209ce
Nelson Durrant <snelsondurrant@gmail.com>
2024-05-06 16:41:28 -0400

    Updated bash script documentation

--- agent.sh
+++ agent.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+##########################################################
+# STARTS THE MICRO-ROS AGENT
+##########################################################
+
 cd ~/microros_ws
 source install/setup.bash
 ros2 run micro_ros_agent micro_ros_agent multiserial --devs "/dev/ttyACM0 /dev/ttyACM1" -b 6000000
--- msg_sync.sh
+++ msg_sync.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-# syncs changes made in the ros2 frost_interfaces package with the teensy_ws
+##########################################################
+# SYNCS FROST_INTERFACES CHANGES WITH THE TEENSY_WS
+# - After running this script, recompile micro-ROS in 
+#   PlatformIO using "msg_update.sh"
+##########################################################
+
 rsync -avc --delete ~/ros2_ws/src/frost_interfaces ~/teensy_ws/sensors/extra_packages
 rsync -avc --delete ~/ros2_ws/src/frost_interfaces ~/teensy_ws/control/extra_packages
