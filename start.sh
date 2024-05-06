09e5a702e3ddbcb270d7d2793c5adea555a744c1
Nelson Durrant <snelsondurrant@gmail.com>
2024-05-06 16:42:58 -0400

    Bash updates

--- start.sh
+++ start.sh
@@ -19,10 +19,14 @@ echo ""
 cd ~/teensy_ws
 bash restart.sh
 
+echo ""
+
 cd ~/microros_ws
 source install/setup.bash
 ros2 run micro_ros_agent micro_ros_agent multiserial --devs "/dev/ttyACM0 /dev/ttyACM1" -b 6000000 &
 sleep 5
+
+echo ""
 
 cd ~/ros2_ws
 source install/setup.bash
