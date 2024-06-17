5290c928d53bc1cad227f95d252973714063e01d
Nelson Durrant <snelsondurrant@gmail.com>
2024-06-17 11:04:53 -0600

    Instruction update

--- msg_sync.sh
+++ msg_sync.sh
@@ -4,6 +4,8 @@
 # SYNCS FROST_INTERFACES CHANGES WITH THE TEENSY_WS
 # - After running this script, recompile micro-ROS in 
 #   PlatformIO using "msg_update.sh"
+# - Don't run this on the Pi -- run it on your own 
+#   machine and pull the changes to the Pi
 ##########################################################
 
 rsync -avc --delete ~/CougarsRPi/src/frost_interfaces ~/CougarsTeensy/sensors/extra_packages
