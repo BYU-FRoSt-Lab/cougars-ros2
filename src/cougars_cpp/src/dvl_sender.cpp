e719835adfb6d67ccf9378d809104ce783804859
Matthew McMurray <mmcmurray123@gmail.com>
2024-08-08 14:50:15 -0600

    edited dvl_sender

--- src/cougars_cpp/src/dvl_sender.cpp
+++ src/cougars_cpp/src/dvl_sender.cpp
@@ -6,6 +6,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "geometry_msgs/msg/TwistWithCovarianceStamped.hpp"
+#include "dvl_msgs/msg/dvl.hpp"
 
 using std::placeholders::_1;
 using namespace std::chrono_literals;
@@ -17,10 +18,12 @@ class DVLSender : public rclcpp::Node
   public:
     DVLSender():Node("dvl_data_sender")
     {
-      publisher_dvl_velocity = this->create_publisher<std_msgs::msg::String>("dvl_velocity", 10);
-      subscriber_dvl_data = this->create_subscription<std_msgs::msg::String>("/dvl/data", 10, std::bind(&DVLSender::dvl_data_callback, this, _1));
+      publisher_dvl_velocity = this->create_publisher<geometry_msgs::msg::TwistWithCovarianceStamped>("dvl_velocity", 10);
+      subscriber_dvl_data = this->create_subscription<dvl_msgs::msg::DVL>("/dvl/data", 10, std::bind(&DVLSender::dvl_data_callback, this, _1));
 
     }
+
+
 
   private:
     
