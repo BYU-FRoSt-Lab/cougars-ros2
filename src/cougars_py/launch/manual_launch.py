537d97f301fc5ab16f61f9d9eae8c2b9b162b0f0
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-08 13:48:21 -0600

    Added MOOS code and DVL to launch files

--- src/cougars_py/launch/manual_launch.py
+++ src/cougars_py/launch/manual_launch.py
@@ -27,4 +27,10 @@ def generate_launch_description():
         #     package='seatrac',
         #     executable='modem'
         # ),
+        launch.actions.DeclareLaunchArgument('ip_address', default_value='192.168.194.95'),
+        launch_ros.actions.Node(
+            package='dvl_a50', 
+            executable='dvl_a50_sensor', 
+            parameters=[{'dvl_ip_address': launch.substitutions.LaunchConfiguration('ip_address')}],
+        ),
     ])
