d2bf92ebe812ff000dabbb0cebe0b4b5e2710200
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-08 13:48:21 -0600

    Added MOOS code and DVL to launch files

--- src/cougars_cpp/src/moos_bridge.cpp
+++ src/cougars_cpp/src/moos_bridge.cpp
@@ -1,46 +1,145 @@
-// REPLACE THIS WITH AN UPDATED VERSION OF MATTHEW'S BRIDGE CODE
-
-#include <chrono>
 #include <functional>
 #include <memory>
-#include <string>
 
+
+// ros2 stuff
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-using namespace std::chrono_literals;
+#include "MOOS/libMOOS/Comms/MOOSMsg.h"
+#include "MOOS/libMOOS/Comms/MOOSCommClient.h"
+#include "MOOS/libMOOS/Comms/MOOSAsyncCommClient.h"
+#include "MOOS/libMOOS/Utils/CommandLineParser.h"
+#include <exception>
 
-/* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
+using std::placeholders::_1;
 
-class MinimalPublisher : public rclcpp::Node
+
+
+
+MOOS::MOOSAsyncCommClient Comms;
+typedef std::vector<CMOOSMsg> MsgVector;
+typedef std::vector<MOOS::ClientCommsStatus> CommsStatusVector;
+
+// MOOS functions
+
+
+
+class MOOSBridge : public rclcpp::Node
 {
-  public:
-    MinimalPublisher()
-    : Node("minimal_publisher"), count_(0)
-    {
-      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-      timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
-    }
+public:
 
-  private:
-    void timer_callback()
-    {
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      publisher_->publish(message);
+  
+  
+  MOOSBridge()
+  : Node("moos_bridge")
+  {
+
+    init_i();
+
+
+   // CMOOSCommClient::Register("NAV_X", 0.0);
+    subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "topic", 10, std::bind(&Bridge::ros_listener, this, _1));
+
+  }
+
+  void init_i(){
+    i = 1;
+  }
+  void increment_i(){
+    i++;
+  }
+  int get_i(){
+    return i;
+  }
+
+
+
+private:
+
+  int i;
+
+  void ros_listener(const std_msgs::msg::String & msg)
+  {
+    increment_i();
+    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+
+
+    // translate message
+
+
+
+    // notify
+
+    if(get_i() % 10 == 0){
+      Comms.Notify("NAV_X", 149.9);
     }
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
+    
+
+  }
+
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
+
+
+
+bool OnConnect(void * pParam){
+      CMOOSCommClient * pC = reinterpret_cast<CMOOSCommClient*>(pParam);
+      
+      pC->Register("X",0.0);
+
+      // from MOOS-IVP to ros base
+      pC->Register("DESIRED_SPEED",0.0);
+      pC->Register("DESIRED_HEADING", 0.0);
+      pC->Register("DESIRED_DEPTH", 0.0);
+      return 0;
+}
+
+bool OnMail(void * pParam){
+
+  CMOOSCommClient* pC = reinterpret_cast<CMOOSCommClient*>(pParam);
+  MOOSMSG_LIST M;
+  pC->Fetch(M);
+  MOOSMSG_LIST::iterator q;
+  for(q=M.begin(); q != M.end(); q++){
+    CMOOSMsg &msg = *q;
+    std::string key = msg.GetKey();
+    std::cout << "\n";
+
+    if(key == "DESIRED_SPEED"){
+      std::cout << "=====PRINTING DESIRED_SPEED=====" << std::endl;
+
+    }
+    else if(key == "DESIRED_HEADING"){
+      std::cout << "=====PRINTING DESIRED_HEADING=====" << std::endl;
+      
+    }
+    else if(key == "DESIRED_DEPTH"){
+      std::cout << "=====PRINTING DESIRED_DEPTH=====" << std::endl;
+      
+    }
+  
+    q->Trace();
+    std::cout << "\n";
+  }
+  return true;
+}
+
 
 int main(int argc, char * argv[])
 {
+
+  // MOOS stuff
+  Comms.SetOnMailCallBack(OnMail,&Comms);
+  Comms.SetOnConnectCallBack(OnConnect,&Comms);
+  Comms.Run("localhost", 9000, "my_connection");
+  
+  // ROS 2 stuff
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<MOOSBridge>());
   rclcpp::shutdown();
+
   return 0;
 }
--- src/cougars_py/launch/moos_launch.py
+++ src/cougars_py/launch/moos_launch.py
@@ -1,4 +1,6 @@
 import launch
+import launch.actions
+import launch.substitutions
 import launch_ros.actions
 
 def generate_launch_description():
@@ -27,4 +29,10 @@ def generate_launch_description():
         #     package='seatrac',
         #     executable='modem'
         # ),
+        launch.actions.DeclareLaunchArgument('ip_address', default_value='192.168.194.95'),
+        launch_ros.actions.Node(
+            package='dvl_a50', 
+            executable='dvl_a50_sensor', 
+            parameters=[{'dvl_ip_address': launch.substitutions.LaunchConfiguration('ip_address')}],
+        ),
     ])
