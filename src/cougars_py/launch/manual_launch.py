3fd3888a8839ac07728fbb4a4fcc3e57150e9cfb
ClaytonSmith314 <claytonsmith3.14@gmail.com>
2024-08-09 17:20:15 -0600

    added modem pinger with config file

--- src/cougars_py/launch/manual_launch.py
+++ src/cougars_py/launch/manual_launch.py
@@ -2,6 +2,7 @@ import launch
 import launch_ros.actions
 
 def generate_launch_description():
+    config_file = "~/config/vehicle_config.yaml"
     return launch.LaunchDescription([
         launch.actions.ExecuteProcess(
             cmd=['ros2', 'bag', 'record', '-s', 'mcap', '-a'],
@@ -23,10 +24,16 @@ def generate_launch_description():
             package='cougars_cpp',
             executable='demo_control'
         ),
-        # launch_ros.actions.Node(
-        #     package='seatrac',
-        #     executable='modem'
-        # ),
+        launch_ros.actions.Node(
+            package='cougars_py',
+            executable='modem_pinger',
+            parameters=[config_file]
+        ),
+        launch_ros.actions.Node(
+            package='seatrac',
+            executable='modem',
+            parameters=[config_file]
+        ),
         launch.actions.DeclareLaunchArgument('ip_address', default_value='192.168.194.95'),
         launch_ros.actions.Node(
             package='dvl_a50', 
