0718edd18c37f12e206a9def2313f741124802f8
Matthew McMurray <mmcmurray123@gmail.com>
2024-08-12 23:48:50 +0000

    modified start script

--- start.sh
+++ start.sh
@@ -27,7 +27,7 @@ cd ~/ros2_ws
 source install/setup.bash
 cd ~/ros2_ws/bag
 # ros2 launch cougars moos_launch.py
-ros2 launch cougars manual_launch.py
+ros2 launch cougars_py manual_launch.py
 
 # enable acoustics on dvl
 ros2 topic pub dvl/config/command dvl_msgs/msg/ConfigCommand "{command: 'Hello, World!', parameter_name : 'acoustic_enabled', parameter_value: true}" --once
