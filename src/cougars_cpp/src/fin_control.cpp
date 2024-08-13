b030f280badb9124f825aed64d6e87555215447f
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-13 11:59:13 -0600

    Updates

--- src/cougars_cpp/src/fin_control.cpp
+++ src/cougars_cpp/src/fin_control.cpp
@@ -80,17 +80,17 @@ public:
     desired_depth_subscription_ =
         this->create_subscription<frost_interfaces::msg::DesiredDepth>(
             "desired_depth", 10,
-            std::bind(&FinControl::depth_callback, this, _1));
+            std::bind(&FinControl::desired_depth_callback, this, _1));
 
     desired_heading_subscription_ =
         this->create_subscription<frost_interfaces::msg::DesiredHeading>(
             "desired_heading", 10,
-            std::bind(&FinControl::heading_callback, this, _1));
+            std::bind(&FinControl::desired_heading_callback, this, _1));
 
     desired_speed_subscription_ =
         this->create_subscription<frost_interfaces::msg::DesiredSpeed>(
             "desired_speed", 10,
-            std::bind(&FinControl::speed_callback, this, _1));
+            std::bind(&FinControl::desired_speed_callback, this, _1));
 
     depth_subscription_ = this->create_subscription<
         geometry_msgs::msg::PoseWithCovarianceStamped>(
@@ -106,33 +106,33 @@ public:
 
     // declare ros timers
     pid_timer_ = this->create_wall_timer(
-        TIMER_PID_PERIOD, std::bind(&FinControl::timer_callback, this));
+        PID_TIMER_PERIOD_STR, std::bind(&FinControl::timer_callback, this));
   }
 
 private:
   void
-  depth_callback(const frost_interfaces::msg::DesiredDepth &depth_msg) const {
-    desired_depth_msg = depth_msg;
+  desired_depth_callback(const frost_interfaces::msg::DesiredDepth &depth_msg) const {
+    self->desired_depth_msg = depth_msg;
   }
 
-  void heading_callback(
+  void desired_heading_callback(
       const frost_interfaces::msg::DesiredHeading &heading_msg) const {
-    desired_heading_msg = heading_msg;
+    self->desired_heading_msg = heading_msg;
   }
 
   void
-  speed_callback(const frost_interfaces::msg::DesiredSpeed &speed_msg) const {
-    desired_speed_msg = speed_msg;
+  desired_speed_callback(const frost_interfaces::msg::DesiredSpeed &speed_msg) const {
+    self->desired_speed_msg = speed_msg;
   }
 
   void depth_callback(
       const geometry_msgs::msg::PoseWithCovarianceStamped &depth_msg) {
-    depth = depth_msg.pose.position.z;
+    self->depth = depth_msg.pose.position.z;
   }
 
   void velocity_callback(
       const geometry_msgs::msg::TwistWithCovarianceStamped &velocity_msg) {
-    x_velocity = velocity_msg.twist.linear.x;
+    self->x_velocity = velocity_msg.twist.linear.x;
   }
 
   void yaw_callback(const dvl_msgs::msg::DVLDR &yaw_msg) { yaw = yaw_msg.yaw; }
@@ -148,11 +148,11 @@ private:
 
     // TODO: reset the dead reckoning on the dvl as soon as we start moving (?)
 
-    depth_pos = myDepthPID.compute(desired_depth_msg->desired_depth, depth);
-    heading_pos =
-        myHeadingPID.compute(desired_heading_msg->desired_heading, yaw);
-    velocity_level =
-        myVelocityPID.compute(desired_speed_msg->desired_speed, x_velocity);
+    self->depth_pos = myDepthPID.compute(self->desired_depth_msg->desired_depth, depth);
+    self->heading_pos =
+        myHeadingPID.compute(self->desired_heading_msg->desired_heading, yaw);
+    self->velocity_level =
+        myVelocityPID.compute(self->desired_speed_msg->desired_speed, x_velocity);
 
     message.fin[0] = depth_pos;
     message.fin[1] = depth_pos; // TODO: counter-rotation offset?
