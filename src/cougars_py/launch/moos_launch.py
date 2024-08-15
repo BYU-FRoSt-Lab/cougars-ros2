47bc2bc08b55ba417b8be63f8039de1b18b7c02f
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-15 11:32:34 -0600

    Updates to launch scripts

--- src/cougars_py/launch/moos_launch.py
+++ src/cougars_py/launch/moos_launch.py
@@ -4,36 +4,12 @@ import launch_ros.actions
 def generate_launch_description():
     config_file = "/home/frostlab/config/vehicle_config.yaml"
     return launch.LaunchDescription([
+        # Start recording all topics to an mcap file
         launch.actions.ExecuteProcess(
             cmd=['ros2', 'bag', 'record', '-s', 'mcap', '-a'],
             output='screen'
         ),
-        launch_ros.actions.Node(
-            package='cougars_cpp',
-            executable='moos_bridge',
-            parameters=[config_file]
-        ),
-        launch_ros.actions.Node(
-            package='cougars_cpp',
-            executable='pid_control',
-            parameters=[config_file]
-        ),
-        launch_ros.actions.Node(
-            package='cougars_cpp',
-            executable='depth_convertor'
-        ),
-        launch_ros.actions.Node(
-            package='cougars_cpp',
-            executable='dvl_convertor'
-        ),
-        # launch_ros.actions.Node(
-        #     package='cougars_py',
-        #     executable='leak_sub'
-        # ),
-        # launch_ros.actions.Node(
-        #     package='cougars_py',
-        #     executable='battery_sub'
-        # ),
+        # Set up the DVL and enable acoustics
         launch.actions.DeclareLaunchArgument('ip_address', default_value='192.168.194.95'),
         launch_ros.actions.Node(
             package='dvl_a50', 
@@ -43,5 +19,35 @@ def generate_launch_description():
         launch.actions.ExecuteProcess(
             cmd=['ros2', 'topic', 'pub', '-1', 'dvl/config/command', 'dvl_msgs/msg/ConfigCommand', '{command: \'set_config\', parameter_name: \'acoustic_enabled\', parameter_value: true\'}', '--once'],
             output='screen'
+        ),
+        # TODO: Add the modem and GPS nodes
+        # Start the data conversion nodes
+        # TODO: Add more of these
+        launch_ros.actions.Node(
+            package='cougars_cpp',
+            executable='depth_convertor'
+        ),
+        launch_ros.actions.Node(
+            package='cougars_cpp',
+            executable='dvl_convertor'
+        ),
+        # Start the control nodes
+        launch_ros.actions.Node(
+            package='cougars_py',
+            executable='leak_sub'
+        ),
+        launch_ros.actions.Node(
+            package='cougars_py',
+            executable='battery_sub'
+        ),
+        launch_ros.actions.Node(
+            package='cougars_cpp',
+            executable='pid_control',
+            parameters=[config_file]
+        ),
+        launch_ros.actions.Node(
+            package='cougars_py',
+            executable='moos_bridge',
+            parameters=[config_file]
         ),
     ])
