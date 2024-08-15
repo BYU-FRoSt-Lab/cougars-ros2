d029b7d96ccc2029e1332fa7bafd914d936b0af2
Matthew McMurray <mmcmurray123@gmail.com>
2024-08-15 08:09:13 -0600

    uncommented all of vehicle status

--- src/cougars_cpp/src/vehicle_status.cpp
+++ src/cougars_cpp/src/vehicle_status.cpp
@@ -45,95 +45,95 @@ auto qos = rclcpp::QoS(
     qos_profile);
 
 class VehicleStatus : public rclcpp::Node {
-// public:
-//   VehicleStatus() : Node("vehicle_status") {
+public:
+  VehicleStatus() : Node("vehicle_status") {
 
-//     x_y_subscription_ = this->create_subscription<
-//         geometry_msgs::msg::PoseWithCovarianceStamped>(
-//         "odometry/filtered", 10, std::bind(&VehicleStatus::x_y_callback, this, _1));
+    x_y_subscription_ = this->create_subscription<
+        geometry_msgs::msg::PoseWithCovarianceStamped>(
+        "odometry/filtered", 10, std::bind(&VehicleStatus::x_y_callback, this, _1));
 
-//     depth_subscription_ = this->create_subscription<
-//         geometry_msgs::msg::PoseWithCovarianceStamped>(
-//         "depth_data", 10, std::bind(&VehicleStatus::depth_callback, this, _1));
+    depth_subscription_ = this->create_subscription<
+        geometry_msgs::msg::PoseWithCovarianceStamped>(
+        "depth_data", 10, std::bind(&VehicleStatus::depth_callback, this, _1));
 
-//     // TODO: change this to 'vehicle_status' listener
-//     velocity_subscription_ = this->create_subscription<
-//         geometry_msgs::msg::TwistWithCovarianceStamped>(
-//         "dvl_velocity", 10,
-//         std::bind(&VehicleStatus::velocity_callback, this, _1));
+    // TODO: change this to 'vehicle_status' listener
+    velocity_subscription_ = this->create_subscription<
+        geometry_msgs::msg::TwistWithCovarianceStamped>(
+        "dvl_velocity", 10,
+        std::bind(&VehicleStatus::velocity_callback, this, _1));
 
-//     orientation_subscription_ = this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
-//         "modem_imu", qos, std::bind(&VehicleStatus::orientation_callback, this, _1));
+    orientation_subscription_ = this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
+        "modem_imu", qos, std::bind(&VehicleStatus::orientation_callback, this, _1));
 
-//     // declare ros timers
-//     update_timer_ = this->create_wall_timer(
-//         UPDATE_TIMER_MS, std::bind(&VehicleStatus::timer_callback, this));
-//   }
+    // declare ros timers
+    update_timer_ = this->create_wall_timer(
+        UPDATE_TIMER_MS, std::bind(&VehicleStatus::timer_callback, this));
+  }
 
-// private:
-//   void x_y_callback(const geometry_msgs::msg::PoseWithCovarianceStamped &x_y_message ){
-//     this->y_pos = x_y_message.pose.pose.position.y;
-//     this->x_pos = x_y_message.pose.pose.position.x;
-//   }
-//   void depth_callback(
-//       const geometry_msgs::msg::PoseWithCovarianceStamped &depth_msg) {
-//     this->depth = depth_msg.pose.pose.position.z;
-//   }
-//   void velocity_callback(
-//       const geometry_msgs::msg::TwistWithCovarianceStamped &velocity_msg) {
-//     this->x_velocity = velocity_msg.twist.twist.linear.x;
-//   }
-//   void orientation_callback(const geometry_msgs::msg::PoseWithCovarianceStamped &orientation_msg) {
-//     this->q_w = orientation_msg.pose.pose.orientation.w;
-//     this->q_x = orientation_msg.pose.pose.orientation.x;
-//     this->q_y = orientation_msg.pose.pose.orientation.y;
-//     this->q_z = orientation_msg.pose.pose.orientation.z;
-//   }
+private:
+  void x_y_callback(const geometry_msgs::msg::PoseWithCovarianceStamped &x_y_message ){
+    this->y_pos = x_y_message.pose.pose.position.y;
+    this->x_pos = x_y_message.pose.pose.position.x;
+  }
+  void depth_callback(
+      const geometry_msgs::msg::PoseWithCovarianceStamped &depth_msg) {
+    this->depth = depth_msg.pose.pose.position.z;
+  }
+  void velocity_callback(
+      const geometry_msgs::msg::TwistWithCovarianceStamped &velocity_msg) {
+    this->x_velocity = velocity_msg.twist.twist.linear.x;
+  }
+  void orientation_callback(const geometry_msgs::msg::PoseWithCovarianceStamped &orientation_msg) {
+    this->q_w = orientation_msg.pose.pose.orientation.w;
+    this->q_x = orientation_msg.pose.pose.orientation.x;
+    this->q_y = orientation_msg.pose.pose.orientation.y;
+    this->q_z = orientation_msg.pose.pose.orientation.z;
+  }
 
 
-//   void update_callback() {
-//     auto message = nav_msgs::msg::Odometry();
-//     message.pose.pose.position.x = this->x_pos;
-//     message.pose.pose.position.y = this->y_pos;
-//     message.pose.pose.position.z = this->depth;
-//     message.pose.pose.orientation.w = this->q_w;
-//     message.pose.pose.orientation.y = this->q_y;
-//     message.pose.pose.orientation.z = this->q_z;
-//     message.pose.pose.orientation.x = this->q_x;
-//     message.twist.twist.linear.x = this->x;
+  void update_callback() {
+    auto message = nav_msgs::msg::Odometry();
+    message.pose.pose.position.x = this->x_pos;
+    message.pose.pose.position.y = this->y_pos;
+    message.pose.pose.position.z = this->depth;
+    message.pose.pose.orientation.w = this->q_w;
+    message.pose.pose.orientation.y = this->q_y;
+    message.pose.pose.orientation.z = this->q_z;
+    message.pose.pose.orientation.x = this->q_x;
+    message.twist.twist.linear.x = this->x;
     
 
-//     // publishes speed, depth, global x,y, 
-//     // and orientation (quaternion)
-//     // to be used by MOOS and anything else to 
-//     vehicle_status_publisher_->publish(message);
+    // publishes speed, depth, global x,y, 
+    // and orientation (quaternion)
+    // to be used by MOOS and anything else to 
+    vehicle_status_publisher_->publish(message);
 
-//   }
+  }
 
-//   // call back timer to update
-//   rclcpp::TimerBase::SharedPtr update_timer_;
-//   // depth, speed, heading
-//   rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr
-//       depth_subscription_;
-//   rclcpp::Subscription<geometry_msgs::msg::TwistWithCovarianceStamped>::
-//       SharedPtr velocity_subscription_;
-//   rclcpp::Subscription<dvl_msgs::msg::DVLDR>::SharedPtr orientation_subscription_;
+  // call back timer to update
+  rclcpp::TimerBase::SharedPtr update_timer_;
+  // depth, speed, heading
+  rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr
+      depth_subscription_;
+  rclcpp::Subscription<geometry_msgs::msg::TwistWithCovarianceStamped>::
+      SharedPtr velocity_subscription_;
+  rclcpp::Subscription<dvl_msgs::msg::DVLDR>::SharedPtr orientation_subscription_;
 
-//   // current x,y
-//   rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr x_y_subscription_;
+  // current x,y
+  rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr x_y_subscription_;
 
-//   rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr
-//       vehicle_status_publisher_;
+  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr
+      vehicle_status_publisher_;
 
-//   // status variables
-//   float q_x = 0.0;
-//   float q_y = 0.0;
-//   float q_z = 0.0;
-//   float q_w = 0.0;
-//   float x_velocity = 0.0;
-//   float depth = 0.0;
-//   float x_pos = 0.0;
-//   float y_pos = 0.0;
+  // status variables
+  float q_x = 0.0;
+  float q_y = 0.0;
+  float q_z = 0.0;
+  float q_w = 0.0;
+  float x_velocity = 0.0;
+  float depth = 0.0;
+  float x_pos = 0.0;
+  float y_pos = 0.0;
 };
 
 int main(int argc, char *argv[]) {
