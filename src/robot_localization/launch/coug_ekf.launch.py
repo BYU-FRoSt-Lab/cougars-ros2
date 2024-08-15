ebfa211c1926014cac4d2f81245658dedd2321c0
ClaytonSmith314 <claytonsmith3.14@gmail.com>
2024-08-15 15:53:47 -0600

    Updated coug_ekf to match our system better

---
+++ src/robot_localization/launch/coug_ekf.launch.py
@@ -0,0 +1,37 @@
+
+
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+import launch_ros.actions
+import os
+import yaml
+from launch.substitutions import EnvironmentVariable
+import pathlib
+import launch.actions
+from launch.actions import DeclareLaunchArgument
+
+def generate_launch_description():
+    pkg_dir = get_package_share_directory("robot_localization")
+    return LaunchDescription([
+        launch_ros.actions.Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters=[os.path.join(pkg_dir, 'params', 'coug_ekf.yaml')],
+            remappings=[
+                ("dvl_dead_reckoning", "/fake/dvl/dead_reckoning")
+            ]
+        ),
+        launch_ros.actions.Node(
+            package='cougars_py',
+            executable='seatrac_ahrs_converter',
+            name='seatrac_ahrs_converter',
+            output='screen'
+        ),
+        launch_ros.actions.Node(
+            package='seatrac',
+            executable='modem'
+        ),
+
+    ])
