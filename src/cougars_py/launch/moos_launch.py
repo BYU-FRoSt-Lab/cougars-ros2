733cc6351a91e63a2196925724dca3f64f0550f8
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-15 11:33:48 -0600

    Fixed a bug

--- src/cougars_py/launch/moos_launch.py
+++ src/cougars_py/launch/moos_launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
             parameters=[{'dvl_ip_address': launch.substitutions.LaunchConfiguration('ip_address')}],
         ),
         launch.actions.ExecuteProcess(
-            cmd=['ros2', 'topic', 'pub', '-1', 'dvl/config/command', 'dvl_msgs/msg/ConfigCommand', '{command: \'set_config\', parameter_name: \'acoustic_enabled\', parameter_value: true\'}', '--once'],
+            cmd=['ros2', 'topic', 'pub', '-1', 'dvl/config/command', 'dvl_msgs/msg/ConfigCommand', '{command: \'set_config\', parameter_name: \'acoustic_enabled\', parameter_value: true}'],
             output='screen'
         ),
         # TODO: Add the modem and GPS nodes
