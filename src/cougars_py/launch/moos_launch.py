097f0a2a1db688ac289fe33b03530e05e2ae5f6d
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-15 13:26:36 -0600

    Updates

--- src/cougars_py/launch/moos_launch.py
+++ src/cougars_py/launch/moos_launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
             executable='dvl_a50_sensor', 
         ),
         launch.actions.ExecuteProcess(
-            cmd=['ros2', 'topic', 'pub', '-10', 'dvl/config/command', 'dvl_msgs/msg/ConfigCommand', '{command: \'set_config\', parameter_name: \'acoustic_enabled\', parameter_value: false}'],
+            cmd=['ros2', 'topic', 'pub', 'dvl/config/command', 'dvl_msgs/msg/ConfigCommand', '{command: \'set_config\', parameter_name: \'acoustic_enabled\', parameter_value: false}', '-t', '10'],
             output='screen'
         ),
         # Setup the USBL modem
