a2de2df281f54d2ea089938832df1badd6fdfdb7
Matthew McMurray <mmcmurray123@gmail.com>
2024-08-20 18:29:23 -0600

    started on gtsam code

--- src/cougars_cpp/src/factor_graph.cpp
+++ src/cougars_cpp/src/factor_graph.cpp
@@ -6,7 +6,155 @@
 #include <gtsam/nonlinear/ISAM2.h>
 #include <gtsam/slam/BetweenFactor.h>
 
+
 using namespace gtsam;
+
+// Agents - each agent creates an array of other agents
+// to stay aware of where the other agents are
+
+class Agent {
+
+    private:
+        Pose3 pose_world_noisy;
+        std::vector<double> x_tr;
+        std::vector<double> y_tr;
+        std::vector<double> z_tr;
+        std::vector<double> x_noisy;
+        std::vector<double> y_noisy;
+        std::vector<double> z_noisy;
+        std::vector<double> roll_tr;
+        std::vector<double> pitch_tr;
+        std::vector<double> yaw_tr;
+        int step;
+        int agentNumber;
+        Pose3 prev_pose_world_noisy_comms;
+        int poseKey;
+        int prevPoseKey;
+        int poseKeyStart;
+    
+    public:
+
+        Agent(){}
+        Agent(Pose3 H_init, int agentNumber){
+
+            // std::cout << "agent number cpp: " << agent_number << std::endl;
+            pose_world_noisy = H_init;
+            prev_pose_world_noisy_comms = H_init;
+            poseKey = (agentNumber + 1) * 1e6;
+            prevPoseKey = poseKey;
+            poseKeyStart = poseKey;
+            agentNumber = agentNumber;
+
+        }
+        ~Agent(){}
+
+
+        void SetXNoisy(std::vector<double> x){
+            x_noisy = x;
+        }
+        std::vector<double> GetXNoisy(){
+            return x_noisy;
+        }
+        void PushBackXNoisy(double value){
+            x_noisy.push_back(value);
+        }
+
+        void SetYNoisy(std::vector<double> x){
+            y_noisy = x;
+        }
+        std::vector<double> GetYNoisy(){
+            return y_noisy;
+        }
+        void PushBackYNoisy(double value){
+            y_noisy.push_back(value);
+        }
+
+        void SetXTrue(std::vector<double> x){
+            x_tr = x;
+        }
+        std::vector<double> GetXTrue(){
+            return x_tr;
+        }
+        void PushBackXTrue(double value){
+            x_tr.push_back(value);
+        }
+        void SetYTrue(std::vector<double> y){
+            y_tr = y;   
+        }
+        std::vector<double> GetYTrue(){
+            return y_tr;
+        }
+        void PushBackYTrue(double value){
+            y_tr.push_back(value);
+        }
+        void SetZTrue(std::vector<double> z){
+            z_tr = z;
+        }
+        std::vector<double> GetZTrue(){
+            return z_tr;
+        }
+        void PushBackZTrue(double value){
+            z_tr.push_back(value);
+        }
+        void SetRollTrue(std::vector<double> r){
+            roll_tr = r;
+        }
+
+        std::vector<double> GetRollTrue(){
+            return roll_tr;
+        }
+
+        void SetPitchTrue(std::vector<double> p){
+            pitch_tr = p;
+        }
+        std::vector<double> GetPitchTrue(){
+            return pitch_tr;
+        }
+        void SetYawTrue(std::vector<double> y){
+            yaw_tr = y;
+        }
+        std::vector<double> GetYawTrue(){
+            return yaw_tr;
+        }
+
+        void updateStep(int nextStep){
+            step = nextStep;
+        }
+
+        void UpdatePoseKey(){
+            prevPoseKey = poseKey;
+            poseKey++;
+        }
+
+        int getStep(){
+            return step;
+        }
+        int getAgentNumber(){
+            return agentNumber;
+        }
+        Pose3 GetPoseWorldNoisy(){
+            return pose_world_noisy;
+        }
+        Pose3 getPrevWorldNoisyComms(){
+            return prev_pose_world_noisy_comms;
+        }
+        void UpdatePrevWorldNoisyComms(){
+            prev_pose_world_noisy_comms = GetPoseWorldNoisy();
+
+        }
+        int getPoseKey(){
+            return poseKey;
+        }
+        int getPrevPoseKey(){
+            return prevPoseKey;
+        }
+        int getPoseKeyStart(){
+            return poseKeyStart;
+        }
+        void SetPoseWorldNoisy(Pose3 new_pose){
+            pose_world_noisy = new_pose;
+        }
+};
 
 class FactorStateNode : public rclcpp::Node
 {
@@ -14,6 +162,19 @@ public:
     FactorStateNode()
         : Node("factor_state_node")
     {
+
+        // gtsam stuff
+        ISAM2Params parameters;
+        parameters.relinearizeThreshold = 0.01;
+        parameters.relinearizeSkip = 1;
+        parameters.cacheLinearizedFactors = false;
+        parameters.enableDetailedResults = true;
+        isam = ISAM2(parameters);
+        sigmas << Vector3::Constant(0.1), Vector3::Constant(0.008);
+        odometryNoise = noiseModel::Diagonal::Sigmas(sigmas);
+        priorFactorInit = true;
+
+
         // Set up the subscriptions
         imu_subscriber_ = this->create_subscription<sensor_msgs::msg::Imu>(
             "/modem_imu", 10,
@@ -32,6 +193,11 @@ public:
     }
 
 private:
+    
+    // this is the agent object to
+    // keep track of where it is at
+    Agent * agent;
+    double theta;
 
     // gtsam stuff
     NonlinearFactorGraph graph;
@@ -41,6 +207,7 @@ private:
     Vector6 std_noise;
     Vector6 sigmas;
     noiseModel::Diagonal::shared_ptr odometryNoise;
+    bool priorFactorInit;
 
 
 
@@ -49,13 +216,65 @@ private:
         result = isam.calculateEstimate();
         graph.resize(0);
         initialEstimate.clear();
+
+
+        /// TODO: publish the latest estimate -- look into not updating the the whole path
+
+
+
+
+
     }
+
+    Pose3 CalculateH(const nav_msgs::msg::Odometry & msg){
+
+            Eigen::Quaterniond q;
+
+    
+            q.x() = msg.pose.pose.orientation.x;
+            q.y() = msg.pose.pose.orientation.y;
+            q.z() = msg.pose.pose.orientation.z;
+            q.w() = msg.pose.pose.orientation.w;
+            
+
+            Eigen::Matrix3d R = q.toRotationMatrix();
+
+            Eigen::Matrix4d H;
+
+            H.block(0, 0, 3, 3) = R;
+            
+
+            H(0,3) = msg.pose.pose.position.x;
+            H(1,3) = msg.pose.pose.position.y;
+            H(2,3) = msg.pose.pose.position.z;
+            
+
+            H(3,0) = 0;
+            H(3,1) = 0;
+            H(3,2) = 0;
+            H(3,3) = 1;
+
+            return Pose3(H);
+
+        }
 
 
 
     // Callback functions for each topic
     void imuCallback(const sensor_msgs::msg::Imu::SharedPtr msg)
     {
+
+
+        /// TODO: get theta and then call the dvl reset (convert back to heading)
+
+        // theta = msg->orientation.
+
+
+
+
+
+
+
         // Process the IMU data here
         RCLCPP_INFO(this->get_logger(), "Received IMU data.");
         // Example: Print the IMU orientation
@@ -66,13 +285,53 @@ private:
         publishFactorState();
     }
 
-    void gpsOdomCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
-    {
+    void gpsOdomCallback(const nav_msgs::msg::Odometry::SharedPtr& msg)
+    {   
+
+        if(priorFactorInit){
+            Pose3 H_init = CalculateH(msg);
+            agent = new Agent(H_init, msg.agent_number);
+            priorFactorInit = false;
+            graph.push_back(PriorFactor<Pose3>(agent->getPoseKey(),agents->GetPoseWorldNoisy(), odometryNoise));
+            initialEstimate.insert(agents->getPoseKey(), agents->GetPoseWorldNoisy());
+
+            update_est();
+
+
+            return;
+
+        }
+
+        /// TODO: add unary factor///
+
+
+
+
+
+
+
+        /////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         // Process the GPS odometry data here
         RCLCPP_INFO(this->get_logger(), "Received GPS odometry data.");
         // Example: Print the position
         RCLCPP_INFO(this->get_logger(), "Position x: %f, y: %f",
-                    msg->pose.pose.position.x, msg->pose.pose.position.y);
+                    msg.pose.pose.position.x, msg.pose.pose.position.y);
 
         // Integrate data processing logic here
         publishFactorState();
@@ -80,6 +339,37 @@ private:
 
     void dvlDeadReckonCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
     {
+
+        
+
+        if(!priorFactorInit){
+
+            // do transformation right here
+
+            Pose3 H_pose2_wrt_pose1_noisy = CalculateH(msg);
+            Pose3 poseWorldNoisy = CalculateH(msg);
+            agent->SetPoseWorldNoisy(poseWorldNoisy);
+            agent->UpdatePoseKey();
+            initialEstimate.insert(agent->getPoseKey(), agent->GetPoseWorldNoisy());
+            graph.add(BetweenFactor<Pose3>(agent->getPrevPoseKey(),agent->getPoseKey(),H_pose2_wrt_pose1_noisy,odometryNoise));
+
+            update_est();
+
+        }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
         // Process the DVL dead reckoning data here
         RCLCPP_INFO(this->get_logger(), "Received DVL dead reckoning data.");
         // Example: Print the velocity
