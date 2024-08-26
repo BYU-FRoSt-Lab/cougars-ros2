3751047848febfcb40f2bd40e26cbd96ed0daffe
Matthew McMurray <mmcmurray123@gmail.com>
2024-08-26 11:24:37 -0600

    fixed ntp script

--- dvl_tools/set_ntp.sh
+++ dvl_tools/set_ntp.sh
@@ -1,4 +1,4 @@
 sudo systemctl restart chrony
 ip_address=$(ifconfig eth0 | grep 'inet ' | awk '{print $2}')
-curl -X POST -d '{"ntp_enabled":true,"ntp_server":"$ip_address","ntp_synchronized":true}' -H "Content-Type: application/json" http://192.168.194.95/api/v1/time/ntp
+curl -X POST -d "{\"ntp_enabled\":true,\"ntp_server\":\"$ip_address\",\"ntp_synchronized\":true}" -H "Content-Type: application/json" http://192.168.194.95/api/v1/time/ntp
 
