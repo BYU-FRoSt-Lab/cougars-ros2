35a10ed8cf340a8c3d6c29ef0ed6e220a2681ad0
Matthew McMurray <mmcmurray123@gmail.com>
2024-08-27 11:10:39 -0600

    debugging graph

--- src/cougars_py/cougars_py/factor_graph.py
+++ src/cougars_py/cougars_py/factor_graph.py
@@ -384,72 +384,162 @@ class FactorGraphNode(Node):
 
     def unary_assignment(self, sensor):
 
-        if sensor == 'gps':
-            in_future = False
-            curr_time = self.dvl_time       #Timestamp of the current pose key added
-            new_id = int(self.agent.poseKey)    #The posekey id that you will start searching at
-            while(len(self.q_gps) > 1 and in_future == False):       #If measurement in queue and the oldest measurment is later than current posekey
-                print("new_id:%d"%new_id)
-                oldest_measurement_time = (self.q_gps[0].header.stamp.sec * 1_000_000_000 + self.q_gps[0].header.stamp.nanosec) 
-                print("oldest measurment time: %d\n"%oldest_measurement_time)
-                next_measurement_time = (self.q_gps[1].header.stamp.sec * 1_000_000_000 + self.q_gps[1].header.stamp.nanosec ) 
-                print("next measurment time: %d\n"%next_measurement_time)
-                print('gps_q > 1')
-                print(len(self.q_gps))
-                if(oldest_measurement_time < curr_time):
-                    print('oldest measurement time < curr time')
+
+        msg_queue = []
+        if sensor == 'depth':
+            # self.get_logger().info("depth unary")
+            msg_queue = self.q_depth # not a copy, the reference
+            last_pose_key = self.imu_last_pose_key
+        elif sensor == 'imu':
+            # self.get_logger().info("imu unary")
+            msg_queue = self.q_imu # not a copy, the reference
+            last_pose_key = self.depth_last_pose_key
+        elif sensor == 'gps':
+            # self.get_logger().info("imu unary")
+            msg_queue = self.q_gps # not a copy, the reference
+            last_pose_key = self.gps_last_pose_key
+
+
+        in_future = False
+        curr_time = self.dvl_time       #Timestamp of the current pose key added
+        new_id = int(self.agent.poseKey)    #The posekey id that you will start searching at
+        while(len(msg_queue) > 1 and in_future == False):       #If measurement in queue and the oldest measurment is later than current posekey
+            print("new_id:%d"%new_id)
+            oldest_measurement_time = (msg_queue[0].header.stamp.sec * 1_000_000_000 + msg_queue[0].header.stamp.nanosec) 
+            print("oldest measurment time: %d\n"%oldest_measurement_time)
+            next_measurement_time = (msg_queue[1].header.stamp.sec * 1_000_000_000 + msg_queue[1].header.stamp.nanosec ) 
+            print("next measurment time: %d\n"%next_measurement_time)
+            print('gps_q > 1')
+            print(len(msg_queue))
+            if(oldest_measurement_time < curr_time):
+                print('oldest measurement time < curr time')
+                
+                newer_key_time = self.poseKey_to_time[int(new_id)] 
+                print("newer key time: %d\n"%newer_key_time)
+                older_key_time = self.poseKey_to_time[int(new_id - 1)]
+                print("older key time: %d\n"% older_key_time)
+                time_to_current = abs(newer_key_time - oldest_measurement_time) 
+                print("time to current: %d\n"%time_to_current)
+                time_to_previous = abs(older_key_time - oldest_measurement_time) 
+                print("time to previous: %d\n"%time_to_previous)
+
+                if(time_to_current > time_to_previous):
+                    print('time to current is longer than time to prev')
+                    new_id -= 1
+                    if(new_id == self.gps_last_pose_key):
+                        print('pop')
+                        msg_queue.pop(0)
+                        new_id = int(self.agent.poseKey)
+                else:
+                    print('time to current is shortest')
+
+                    time_old_to_pose = abs(oldest_measurement_time - newer_key_time)
+                    print("time old to pose: %d\n"%time_old_to_pose)
+                    time_next_to_pose = abs(next_measurement_time - newer_key_time)
+                    print("time next to pose: %d\n"%time_old_to_pose)
+
+                    if(next_measurement_time < newer_key_time):
+                    # Take care of where next measurement is not past next node
+                        msg_queue.pop(0)
                     
-                    newer_key_time = self.poseKey_to_time[int(new_id)] 
-                    print("newer key time: %d\n"%newer_key_time)
-                    older_key_time = self.poseKey_to_time[int(new_id - 1)]
-                    print("older key time: %d\n"% older_key_time)
-                    time_to_current = abs(newer_key_time - oldest_measurement_time) 
-                    print("time to current: %d\n"%time_to_current)
-                    time_to_previous = abs(older_key_time - oldest_measurement_time) 
-                    print("time to previous: %d\n"%time_to_previous)
+                    elif(time_old_to_pose > time_next_to_pose):
+                    # Take care of case where next measurement is better
+                        msg_queue.pop(0)
+                        new_id = self.agent.poseKey
 
-                    if(time_to_current > time_to_previous):
-                        print('time to current is longer than time to prev')
-                        new_id -= 1
-                        if(new_id == self.gps_last_pose_key):
-                            print('pop')
-                            self.q_gps.pop(0)
-                            new_id = int(self.agent.poseKey)
                     else:
-                        print('time to current is shortest')
+                        #Actually add the gps factor
+                        gps_msg = msg_queue.pop(0)
 
-                        time_old_to_pose = abs(oldest_measurement_time - newer_key_time)
-                        print("time old to pose: %d\n"%time_old_to_pose)
-                        time_next_to_pose = abs(next_measurement_time - newer_key_time)
-                        print("time next to pose: %d\n"%time_old_to_pose)
-
-                        if(next_measurement_time < newer_key_time):
-                        # Take care of where next measurement is not past next node
-                            self.q_gps.pop(0)
+                        gps_meas = gtsam.Point3(gps_msg.pose.pose.position.x, gps_msg.pose.pose.position.y, gps_msg.pose.pose.position.z)
+                        self.graph.add(gtsam.CustomFactor(self.GPS_NOISE, [new_id], partial(self.error_gps, gps_meas)))
+                        self.get_logger().info("added gps unary")
                         
-                        elif(time_old_to_pose > time_next_to_pose):
-                        # Take care of case where next measurement is better
-                            self.q_gps.pop(0)
-                            new_id = self.agent.poseKey
 
-                        else:
-                            #Actually add the gps factor
-                            gps_msg = self.q_gps.pop(0)
+                        #plot
+                        time = gps_msg.header.stamp.nanosec + gps_msg.header.stamp.sec * 1e9
+                        self.plot.add_measurement(gps_msg.pose.pose.position.x,time, new_id )
 
-                            gps_meas = gtsam.Point3(gps_msg.pose.pose.position.x, gps_msg.pose.pose.position.y, gps_msg.pose.pose.position.z)
-                            self.graph.add(gtsam.CustomFactor(self.GPS_NOISE, [new_id], partial(self.error_gps, gps_meas)))
-                            self.get_logger().info("added gps unary")
+                        self.gps_last_pose_key = new_id
+                        new_id = self.agent.poseKey
+
+            else:
+                in_future = True
+
+
+        if sensor == 'depth':
+            self.depth_last_pose_key = int(last_pose_key)
+        elif sensor == 'imu':
+            self.imu_last_pose_key = int(last_pose_key)
+        elif sensor == 'gps':
+            self.gps_last_pose_key = int(last_pose_key)    
+
+        # if sensor == 'gps':
+            # in_future = False
+            # curr_time = self.dvl_time       #Timestamp of the current pose key added
+            # new_id = int(self.agent.poseKey)    #The posekey id that you will start searching at
+            # while(len(self.q_gps) > 1 and in_future == False):       #If measurement in queue and the oldest measurment is later than current posekey
+            #     print("new_id:%d"%new_id)
+            #     oldest_measurement_time = (self.q_gps[0].header.stamp.sec * 1_000_000_000 + self.q_gps[0].header.stamp.nanosec) 
+            #     print("oldest measurment time: %d\n"%oldest_measurement_time)
+            #     next_measurement_time = (self.q_gps[1].header.stamp.sec * 1_000_000_000 + self.q_gps[1].header.stamp.nanosec ) 
+            #     print("next measurment time: %d\n"%next_measurement_time)
+            #     print('gps_q > 1')
+            #     print(len(self.q_gps))
+            #     if(oldest_measurement_time < curr_time):
+            #         print('oldest measurement time < curr time')
+                    
+            #         newer_key_time = self.poseKey_to_time[int(new_id)] 
+            #         print("newer key time: %d\n"%newer_key_time)
+            #         older_key_time = self.poseKey_to_time[int(new_id - 1)]
+            #         print("older key time: %d\n"% older_key_time)
+            #         time_to_current = abs(newer_key_time - oldest_measurement_time) 
+            #         print("time to current: %d\n"%time_to_current)
+            #         time_to_previous = abs(older_key_time - oldest_measurement_time) 
+            #         print("time to previous: %d\n"%time_to_previous)
+
+            #         if(time_to_current > time_to_previous):
+            #             print('time to current is longer than time to prev')
+            #             new_id -= 1
+            #             if(new_id == self.gps_last_pose_key):
+            #                 print('pop')
+            #                 self.q_gps.pop(0)
+            #                 new_id = int(self.agent.poseKey)
+            #         else:
+            #             print('time to current is shortest')
+
+            #             time_old_to_pose = abs(oldest_measurement_time - newer_key_time)
+            #             print("time old to pose: %d\n"%time_old_to_pose)
+            #             time_next_to_pose = abs(next_measurement_time - newer_key_time)
+            #             print("time next to pose: %d\n"%time_old_to_pose)
+
+            #             if(next_measurement_time < newer_key_time):
+            #             # Take care of where next measurement is not past next node
+            #                 self.q_gps.pop(0)
+                        
+            #             elif(time_old_to_pose > time_next_to_pose):
+            #             # Take care of case where next measurement is better
+            #                 self.q_gps.pop(0)
+            #                 new_id = self.agent.poseKey
+
+            #             else:
+            #                 #Actually add the gps factor
+            #                 gps_msg = self.q_gps.pop(0)
+
+            #                 gps_meas = gtsam.Point3(gps_msg.pose.pose.position.x, gps_msg.pose.pose.position.y, gps_msg.pose.pose.position.z)
+            #                 self.graph.add(gtsam.CustomFactor(self.GPS_NOISE, [new_id], partial(self.error_gps, gps_meas)))
+            #                 self.get_logger().info("added gps unary")
                             
 
-                            #plot
-                            time = gps_msg.header.stamp.nanosec + gps_msg.header.stamp.sec * 1e9
-                            self.plot.add_measurement(gps_msg.pose.pose.position.x,time, new_id )
+            #                 #plot
+            #                 time = gps_msg.header.stamp.nanosec + gps_msg.header.stamp.sec * 1e9
+            #                 self.plot.add_measurement(gps_msg.pose.pose.position.x,time, new_id )
 
-                            self.gps_last_pose_key = new_id
-                            new_id = self.agent.poseKey
+            #                 self.gps_last_pose_key = new_id
+            #                 new_id = self.agent.poseKey
 
-                else:
-                    in_future = True
+            #     else:
+            #         in_future = True
 
 
         # elif sensor == 'depth' and len(self.q_depth) > 1 or sensor == 'imu' and len(self.q_imu) > 1:
