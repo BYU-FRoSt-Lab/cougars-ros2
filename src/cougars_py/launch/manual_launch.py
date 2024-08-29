0b2318d9d625a9d4c348f109f8eebf97a5a90c00
Braden Meyers <bjz-github@spwiz.com>
2024-08-29 13:32:32 -0600

    restructured launch files

--- src/cougars_py/launch/manual_launch.py
+++ src/cougars_py/launch/manual_launch.py
@@ -1,83 +1,29 @@
 import launch
 import launch_ros.actions
 import launch_ros.descriptions
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription
+from ament_index_python.packages import get_package_share_directory
+
 
 import os
-import yaml
-import datetime
 
 
-def generate_launch_description():
-
-    folder_exists = True
-    while folder_exists:
-        folder = input("Enter a new descriptive folder name: ")
-        folder = folder + "_" + str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
-        if not os.path.exists("/home/frostlab/ros2_ws/bag/" + folder):
-            folder_exists = False
-    
+def generate_launch_description():   
     config_file = "/home/frostlab/config/vehicle_config.yaml"
-    with open(config_file, 'r') as f:
-        vehicle_config_params = yaml.safe_load(f)
+
+    # Get the directory of the launch files
+    package_dir = os.path.join(
+        get_package_share_directory('cougars_py'), 'launch')
+    
+    # Include additional launch files
+    sensors_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(package_dir, 'sensors_launch.py'))
+    )
 
     return launch.LaunchDescription([
-        launch.actions.ExecuteProcess(
-            cmd=['ros2', 'bag', 'record', '-o', '/home/frostlab/ros2_ws/bag/' + folder, '-s', 'mcap', '-a'],
-            output='screen',
-        ),
-        # Set up the DVL
-        launch_ros.actions.Node(
-            package='dvl_a50', 
-            executable='dvl_a50_sensor', 
-        ),
-        # Setup the USBL modem
-        launch_ros.actions.Node(
-            package='seatrac',
-            executable='modem',
-            parameters=[config_file],
-        ),
-        # Setup the GPS
-        launch_ros.actions.ComposableNodeContainer(
-            package='rclcpp_components',
-            executable='component_container',
-            name='fix_and_odometry_container',
-            namespace='',
-            composable_node_descriptions=[
-                launch_ros.descriptions.ComposableNode(
-                    package='gpsd_client',
-                    plugin='gpsd_client::GPSDClientComponent',
-                    name='gpsd_client',
-                    parameters=[vehicle_config_params['/gpsd_client']['ros__parameters']]),
-                launch_ros.descriptions.ComposableNode(
-                    package='gps_tools',
-                    plugin='gps_tools::UtmOdometryComponent',
-                    name='utm_gpsfix_to_odometry_node')
-            ],
-        ),
-        # Start the data conversion nodes
-        launch_ros.actions.Node(
-            package='cougars_cpp',
-            executable='depth_convertor',
-        ),
-        launch_ros.actions.Node(
-            package='cougars_cpp',
-            executable='dvl_convertor',
-        ),
-        launch_ros.actions.Node(
-            package='cougars_py',
-            executable='seatrac_ahrs_converter',
-        ),
-        launch_ros.actions.Node(
-            package='cougars_py',
-            executable='gps_odom',
-            name='gps_odom',
-            parameters=[config_file],
-        ),
-        # launch_ros.actions.Node(
-        #     package='cougars_cpp',
-        #     executable='vehicle_status',
-        # ),
-        # Start the control nodes
+        sensors_launch,
+
         launch_ros.actions.Node(
             package='cougars_cpp',
             executable='pid_control',
