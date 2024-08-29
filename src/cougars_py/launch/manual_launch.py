5fd129197fe1a6aa02a4d885be64e96471bf6d2d
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-29 17:24:58 -0600

    Updates

--- src/cougars_py/launch/manual_launch.py
+++ src/cougars_py/launch/manual_launch.py
@@ -7,7 +7,6 @@ from ament_index_python.packages import get_package_share_directory
 
 import os
 import datetime
-import yaml
 
 def generate_launch_description():
 
@@ -17,6 +16,7 @@ def generate_launch_description():
     # Get the directory of the launch files
     package_dir = os.path.join(
         get_package_share_directory('cougars_py'), 'launch')
+
 
     return launch.LaunchDescription([
         
