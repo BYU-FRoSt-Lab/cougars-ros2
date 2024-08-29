de7f356aacc2177e12a551a653fdc1809564c5f5
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-29 16:29:57 -0600

    Updates

--- src/cougars_py/launch/manual_launch.py
+++ src/cougars_py/launch/manual_launch.py
@@ -7,25 +7,21 @@ import launch_ros.descriptions
 from ament_index_python.packages import get_package_share_directory
 import os
 import datetime
-import sys
-
-for arg in sys.argv:
-    if arg.startswith("folder:="):
-        folder = arg.split(":=")[1]
+import yaml
 
 def generate_launch_description():
 
-    folder_exists = True
-    while folder_exists:
-        folder = folder + "_" + str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
-        if not os.path.exists("/home/frostlab/ros2_ws/bag/" + folder):
-            folder_exists = False
+    config_file = "/home/frostlab/config/vehicle_config.yaml"
+    with open(config_file, 'r') as f:
+        vehicle_config_params = yaml.safe_load(f)
+
+    # Get folder name from config file
+    folder = vehicle_config_params['folder_name']
+    folder = folder + "_manual_" + str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
 
     # Get the directory of the launch files
     package_dir = os.path.join(
         get_package_share_directory('cougars_py'), 'launch')
-
-    config_file = "/home/frostlab/config/vehicle_config.yaml"
 
     return launch.LaunchDescription([
         
