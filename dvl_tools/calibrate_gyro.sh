1ed5106f03cd4edd650d0359083fc5cd0b8101f8
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-30 13:27:55 -0600

    Added header

--- dvl_tools/calibrate_gyro.sh
+++ dvl_tools/calibrate_gyro.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 echo -n '{"command": "calibrate_gyro"}' | nc -q 0 192.168.194.95 16171 
--- dvl_tools/set_ntp.sh
+++ dvl_tools/set_ntp.sh
@@ -1,7 +1,4 @@
-source /home/frostlab/CougarsSetup/config/rpi_id.sh
-
 #!/bin/bash
 
-source rpi_id.sh
+source /home/frostlab/CougarsSetup/config/rpi_id.sh
 curl -X POST -d "{\"ntp_enabled\":true,\"ntp_server\":\"$STATIC_IP\",\"ntp_synchronized\":true}" -H "Content-Type: application/json" http://192.168.194.95/api/v1/time/ntp
-
