fffd8eb440d4ca72b032874243ea4180007e3d45
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-30 13:06:16 -0600

    Updates

--- start.sh
+++ start.sh
@@ -41,7 +41,6 @@ if [ -z "$(tycmd list)" ]; then
 else 
     cd ~/microros_ws
     source install/setup.bash
-    # ros2 run micro_ros_agent micro_ros_agent multiserial --devs "/dev/ttyACM0 /dev/ttyACM1" -b 6000000
     ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyACM0 -b 6000000 &
 
     sleep 5
