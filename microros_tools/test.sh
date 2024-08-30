5e5eb8e2e9519ff59eee4fc9efc4c88653a53c6a
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-30 11:12:12 -0600

    Updates to microROS

--- microros_tools/test.sh
+++ microros_tools/test.sh
@@ -15,11 +15,19 @@ cleanup() {
 }
 trap cleanup SIGINT
 
-cd ~/microros_ws
-source install/setup.bash
-# ros2 run micro_ros_agent micro_ros_agent multiserial --devs "/dev/ttyACM0 /dev/ttyACM1" -b 6000000 &
-ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyACM0 -b 6000000 &
-sleep 5
+if [ -z "$(tycmd list)" ]; then
+    echo ""
+    echo "ERROR: No Teensy boards avaliable to connect to"
+    echo ""
+
+    exit 1
+
+else 
+    cd ~/microros_ws
+    source install/setup.bash
+    # ros2 run micro_ros_agent micro_ros_agent multiserial --devs "/dev/ttyACM0 /dev/ttyACM1" -b 6000000
+    ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyACM0 -b 6000000 &
+fi
 
 cd ~/ros2_ws
 source install/setup.bash
