4a854bf857c157c1495d606e806a911841a89d05
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-30 13:32:23 -0600

    Updates

--- dvl_tools/set_ntp.sh
+++ dvl_tools/set_ntp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source /home/frostlab/CougarsSetup/config/rpi_id.sh
+source ~/config/network_id.sh
 curl -X POST -d "{\"ntp_enabled\":true,\"ntp_server\":\"$STATIC_IP\",\"ntp_synchronized\":true}" -H "Content-Type: application/json" http://192.168.194.95/api/v1/time/ntp
