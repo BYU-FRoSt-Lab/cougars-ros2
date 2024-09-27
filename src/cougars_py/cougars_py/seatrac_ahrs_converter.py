1cf4651c1c83738fee36efc45cfc8c4692c27b66
ClaytonSmith314 <claytonsmith3.14@gmail.com>
2024-09-27 12:18:01 -0600

    Fixed an outdated comment

--- src/cougars_py/cougars_py/seatrac_ahrs_converter.py
+++ src/cougars_py/cougars_py/seatrac_ahrs_converter.py
@@ -32,7 +32,8 @@ class SeatracAHRSConverter(Node):
             modem_imu.header.stamp = self.get_clock().now().to_msg()
 
             yaw   = 0.1 * msg.attitude_yaw + self.get_parameter('magnetic_declination').get_parameter_value().double_value
-            # since the declination is East, we subtract it from the yaw
+            # The declination is East, but Z axis is down before converting from NED to ENU
+            # so they cancel and magnetic declination is added
             pitch = 0.1 * msg.attitude_pitch
             roll  = 0.1 * msg.attitude_roll
 
