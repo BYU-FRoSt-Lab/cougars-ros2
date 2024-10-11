9b0543cce9b2784d04c78a0111633c6b4e01e950
Nelson Durrant <snelsondurrant@gmail.com>
2024-10-11 12:00:27 -0600

    Bash script updates

--- src/cougars_localization/docs/generate.sh
+++ src/cougars_localization/docs/generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Created by Nelson Durrant, Oct 2024
 #
-# Builds documentation for the ROS2 cougars_localization package
+# Generates documentation for the ROS2 cougars_localization package
 # - The documentation can be accessed by opening the 'index.html'
 #   file in the 'docs_output/cougars_localization' directory
 
