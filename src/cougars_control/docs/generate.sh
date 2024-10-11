b8687b756f90e91fe9aaae34718ba5d736b395ab
Nelson Durrant <snelsondurrant@gmail.com>
2024-10-11 12:00:27 -0600

    Bash script updates

--- src/cougars_control/docs/generate.sh
+++ src/cougars_control/docs/generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Created by Nelson Durrant, Oct 2024
 #
-# Builds documentation for the ROS2 cougars_control package
+# Generates documentation for the ROS2 cougars_control package
 # - The documentation can be accessed by opening the 'index.html'
 #   file in the 'docs_output/cougars_control' directory
 
