adac60f603c0d02d8caf39490e569190eed6d0d4
Nelson Durrant <snelsondurrant@gmail.com>
2024-10-18 12:46:27 -0600

    Scripting updates

--- test.sh
+++ test.sh
@@ -4,6 +4,18 @@
 # Enables acoustics and tests vehicle sensors
 # - Run this after running the 'start.sh' script
 
+function printInfo {
+  echo -e "\033[0m\033[36m[INFO] $1\033[0m"
+}
+
+function printWarning {
+  echo -e "\033[0m\033[33m[WARNING] $1\033[0m"
+}
+
+function printError {
+  echo -e "\033[0m\033[31m[ERROR] $1\033[0m"
+}
+
 source ~/ros2_ws/install/setup.bash
 
 # Turn on DVL acoustics
