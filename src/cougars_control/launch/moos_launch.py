554fc8144a335d64264e190a7ca3540844a80939
Nelson Durrant <snelsondurrant@gmail.com>
2024-10-30 17:41:25 -0600

    Namespace updates

--- src/cougars_control/launch/moos_launch.py
+++ src/cougars_control/launch/moos_launch.py
@@ -1,6 +1,7 @@
 import launch
 import launch_ros.actions
 import launch_ros.descriptions
+from launch.substitutions import LaunchConfiguration
 
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
@@ -26,6 +27,12 @@ def generate_launch_description():
     
     return launch.LaunchDescription([
         
+        # Define the namespace parameter
+        launch.actions.DeclareLaunchArgument(
+            'namespace',
+            default_value='',
+            description='Unique vehicle namespace'
+        ),
         # Include additional launch files
         launch.actions.IncludeLaunchDescription(
             PythonLaunchDescriptionSource(os.path.join(package_dir, 'sensors_launch.py'))
@@ -35,26 +42,31 @@ def generate_launch_description():
             package='cougars_control',
             executable='coug_kinematics',
             parameters=[config_file],
+            namespace=LaunchConfiguration('namespace'),
         ),
         launch_ros.actions.Node(
             package='cougars_control',
             executable='coug_controls',
             parameters=[config_file],
+            namespace=LaunchConfiguration('namespace'),
         ),
         launch_ros.actions.Node(
             package='cougars_control',
             executable='moos_bridge',
             parameters=[config_file],
+            namespace=LaunchConfiguration('namespace'),
             output='screen',
         ),
         # Start the EmergencyStop checks
         # launch_ros.actions.Node(
         #     package='cougars_control',
         #     executable='leak_sub.py',
+        #     namespace=LaunchConfiguration('namespace'),
         # ),
         # launch_ros.actions.Node(
         #     package='cougars_control',
         #     executable='battery_sub.py',
         #     parameters=[config_file],
+        #     namespace=LaunchConfiguration('namespace'),
         # ),
     ])
