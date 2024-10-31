79d11b19b23dcb521657cd86e9e120ddb09a5fbb
bradenMeyers <bjz-github@spwiz.com>
2024-10-31 12:50:10 -0600

    name space cougars sim

--- src/cougars_control/launch/sim_launch.py
+++ src/cougars_control/launch/sim_launch.py
@@ -1,6 +1,7 @@
 import launch
 import launch_ros.actions
 import launch_ros.descriptions
+from launch.substitutions import LaunchConfiguration
 
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
@@ -26,20 +27,29 @@ def generate_launch_description():
 
     return launch.LaunchDescription([
         
+        # Define the namespace parameter
+        launch.actions.DeclareLaunchArgument(
+            'namespace',
+            default_value='',
+            description='Unique vehicle namespace'
+        ),
         # Start the control nodes
         launch_ros.actions.Node(
             package='cougars_control',
             executable='coug_kinematics',
             parameters=[config_file],
+            namespace=LaunchConfiguration('namespace'),
         ),
         launch_ros.actions.Node(
             package='cougars_control',
             executable='coug_controls',
             parameters=[config_file],
+            namespace=LaunchConfiguration('namespace'),
         ),
         launch_ros.actions.Node(
             package='cougars_control',
             executable='manual_mission.py',
             parameters=[config_file],
+            namespace=LaunchConfiguration('namespace'),
         ),
     ])
