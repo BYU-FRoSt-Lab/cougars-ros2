6f4b8709032f748ee76571fc2347d8ba8adc66bd
bradenMeyers <braden@themeyers.net>
2024-11-01 13:21:00 -0600

    logging error

--- src/cougars_control/src/coug_controls.cpp
+++ src/cougars_control/src/coug_controls.cpp
@@ -452,8 +452,12 @@ private:
         // Step 4: Convert quaternion error to Euler angles to get pitch and yaw errors
         Eigen::Vector3d euler_err = q_err.toRotationMatrix().eulerAngles(0, 1, 2);  // roll, pitch, yaw order
 
+        double roll_err = euler_err[0];  // Pitch error in radians
         double pitch_err = euler_err[1];  // Pitch error in radians
         double yaw_err = euler_err[2];    // Yaw error in radians
+
+        RCLCPP_INFO(this->get_logger(), "Yaw Error: %f, Pitch Err: %f, Roll Err: %f",
+                yaw_err, pitch_err, roll_err);
 
         // Step 5: Apply PID control to pitch and heading errors
         int depth_pos = myDepthPID.compute(0, pitch_err * 180.0 / M_PI);  // Convert to degrees if needed
