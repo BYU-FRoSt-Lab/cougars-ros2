787c1480f1063e80fbf8e4ccd6593cf160088a22
Nelson Durrant <snelsondurrant@gmail.com>
2024-11-08 16:04:57 -0700

    Launch file fix and testing script updates

--- src/cougars_localization/launch/sensors_launch.py
+++ src/cougars_localization/launch/sensors_launch.py
@@ -14,6 +14,15 @@ def generate_launch_description():
 
     :return: The launch description.
     '''
+
+    config_file = LaunchConfiguration('config_file')
+    namespace = LaunchConfiguration('namespace')
+
+    # TODO: Figure out how to abstract this
+    config_file_path = '/home/frostlab/config/vehicle_params.yaml'
+
+    with open(config_file_path, 'r') as f:
+        vehicle_config_params = yaml.safe_load(f)
     
     return launch.LaunchDescription([
         
@@ -39,32 +48,32 @@ def generate_launch_description():
         launch_ros.actions.Node(
             package='dvl_a50', 
             executable='dvl_a50_sensor', 
-            namespace=LaunchConfiguration('namespace'),
+            namespace=namespace,
         ),
         # Setup the USBL modem
         launch_ros.actions.Node(
             package='seatrac',
             executable='modem',
-            parameters=[LaunchConfiguration('config_file')],
-            namespace=LaunchConfiguration('namespace'),
+            parameters=[config_file],
+            namespace=namespace,
         ),
         # Setup the GPS
         launch_ros.actions.ComposableNodeContainer(
             package='rclcpp_components',
             executable='component_container',
             name='fix_and_odometry_container',
-            namespace=LaunchConfiguration('namespace'),
+            namespace=namespace,
             composable_node_descriptions=[
                 launch_ros.descriptions.ComposableNode(
                     package='gpsd_client',
                     plugin='gpsd_client::GPSDClientComponent',
                     name='gpsd_client',
-                    namespace=LaunchConfiguration('namespace'),
-                    parameters=[yaml.safe_load(open(LaunchConfiguration('config_file'), 'r'))['/gpsd_client']['ros__parameters']]),
+                    namespace=namespace,
+                    parameters=[vehicle_config_params['coug0']['gpsd_client']['ros__parameters']]), # TODO: Figure out how to abstract this
                 launch_ros.descriptions.ComposableNode(
                     package='gps_tools',
                     plugin='gps_tools::UtmOdometryComponent',
-                    namespace=LaunchConfiguration('namespace'),
+                    namespace=namespace,
                     name='utm_gpsfix_to_odometry_node'),
             ],
         ),
@@ -72,25 +81,25 @@ def generate_launch_description():
         launch_ros.actions.Node(
             package='cougars_localization',
             executable='depth_convertor',
-            parameters=[LaunchConfiguration('config_file')],
-            namespace=LaunchConfiguration('namespace'),
+            parameters=[config_file],
+            namespace=namespace,
         ),
         launch_ros.actions.Node(
             package='cougars_localization',
             executable='dvl_convertor',
-            parameters=[LaunchConfiguration('config_file')],
-            namespace=LaunchConfiguration('namespace'),
+            parameters=[config_file],
+            namespace=namespace,
         ),
         launch_ros.actions.Node(
             package='cougars_localization',
             executable='seatrac_ahrs_convertor',
-            parameters=[LaunchConfiguration('config_file')],
-            namespace=LaunchConfiguration('namespace'),
+            parameters=[config_file],
+            namespace=namespace,
         ),
         launch_ros.actions.Node(
             package='cougars_localization',
             executable='gps_odom.py',
-            parameters=[LaunchConfiguration('config_file')],
-            namespace=LaunchConfiguration('namespace'),
+            parameters=[config_file],
+            namespace=namespace,
         ),
     ])
