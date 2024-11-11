471b68e00fb9dd742c5e4de5ce727a4586fe2100
Nelson Durrant <snelsondurrant@gmail.com>
2024-11-11 11:54:09 -0700

    testing

--- test.sh
+++ test.sh
@@ -82,7 +82,7 @@ else
   fi
 fi
 
-depth_data=$(timeout 3 ros2 topic echo --once --no-arr $NAMESPACE/depth_data 2>/dev/null | grep -oP '(?<=z: )\d+(\.\d+)?')
+depth_data=$(timeout 3 ros2 topic echo --once --no-arr $NAMESPACE/depth_data 2>/dev/null | awk '/position:/{getline; print $3}')
 if [ -z "$depth_data" ]; then
   printError "No depth sensor connection found."
 else
