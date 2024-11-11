5f12adb3781c2595ad64c09075c850e4c6df2292
Nelson Durrant <snelsondurrant@gmail.com>
2024-11-11 10:46:02 -0700

    Naming updates

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
 # Enables acoustics and tests vehicle sensors
 # - Run this after running the 'start.sh' script
 
-source ~/config/bash_params.sh
+source ~/config/bash_vars.sh
 
 function printInfo {
   echo -e "\033[0m\033[36m[INFO] $1\033[0m"
