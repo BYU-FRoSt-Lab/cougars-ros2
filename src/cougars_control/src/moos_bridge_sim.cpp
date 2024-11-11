c109b5d647e17ff0e1b6c18f2c52182d26d5b786
bradenMeyers <bjz-github@spwiz.com>
2024-11-11 14:50:28 -0700

    WIP testing moos with no heading passed in

--- src/cougars_control/src/moos_bridge_sim.cpp
+++ src/cougars_control/src/moos_bridge_sim.cpp
@@ -112,7 +112,7 @@ private:
       nav_heading = (msg.vector.z);
     }
 
-    Comms.Notify("NAV_HEADING", nav_heading);
+    // Comms.Notify("NAV_HEADING", nav_heading);
   }
 
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_vehicle_status_;
@@ -143,20 +143,21 @@ void PublishDesiredValue(double value, std::string key) {
     auto message = frost_interfaces::msg::DesiredSpeed();
     message.desired_speed = value;
     desired_speed_publisher_->publish(message);
-    std::cout << "[INFO] Desired speed" << message.desired_speed << std::endl;
+    std::cout << "[INFO] Desired speed: " << message.desired_speed << std::endl;
   } else if (key == "DESIRED_HEADING") {
     auto message = frost_interfaces::msg::DesiredHeading();
     message.desired_heading = 90 - value;
     if (message.desired_heading < -180.0) {
       message.desired_heading = message.desired_heading + 360;
+    } 
+    std::cout << "[INFO] Desired heading: " << message.desired_heading << std::endl;
     desired_heading_publisher_->publish(message);
-    std::cout << "[INFO] Desired heading" << message.desired_heading << std::endl;
-  } else if (key == "DESIRED_DEPTH") {
+  }
+  else if (key == "DESIRED_DEPTH") {
     auto message = frost_interfaces::msg::DesiredDepth();
     message.desired_depth = value;
     desired_depth_publisher_->publish(message);
-    std::cout << "[INFO] Desired depth" << message.desired_depth << std::endl;
-  }
+    std::cout << "[INFO] Desired depth: " << message.desired_depth << std::endl;
   }
 }
 
