635559050a08df2b1799774a95f9b43e2b1151b6
bradenMeyers <braden@themeyers.net>
2024-11-12 13:17:15 -0700

    gps instead of factor graph test launches

--- src/cougars_control/src/moos_bridge_gps.cpp
+++ src/cougars_control/src/moos_bridge_gps.cpp
@@ -52,11 +52,6 @@ public:
 
 
     // vehicle status listener from the factor graph filter
-    // subscription_vehicle_status_ =
-    //     this->create_subscription<nav_msgs::msg::Odometry>(
-    //         "smoothed_output", 10,
-    //         std::bind(&MOOSBridge::ros_vehicle_status_listener, this, _1));
-    // TODO: this comment above is changing the factor graph to the gps directly
     subscription_vehicle_status_ =
         this->create_subscription<nav_msgs::msg::Odometry>(
             "gps_odom", 10,
@@ -86,10 +81,10 @@ private:
 
     double nav_x, nav_y, nav_depth, nav_speed;
 
-    nav_x = -msg.pose.pose.position.y;
-    nav_y = msg.pose.pose.position.x;
+    nav_x = msg.pose.pose.position.x;
+    nav_y = msg.pose.pose.position.y;
     // nav_depth = msg.pose.pose.position.z;
-    nav_speed = 800.0;
+    nav_speed = 0;
 
     // RCLCPP_INFO(this->get_logger(), "[INFO] Nav x recieved %f, y: %f", nav_x, nav_y);
 
@@ -99,9 +94,7 @@ private:
     // Comms.Notify("NAV_DEPTH", nav_depth);
     Comms.Notify("NAV_SPEED", nav_speed);
 
-    std::cout << "(MOOS) Nav X: " << nav_x << "Nav Y: " << nav_y;
-    std::cout << " --- (HOLOOCEAN) Nav X: " << msg.pose.pose.position.x << "Nav Y: " << msg.pose.pose.position.y << std::endl;
-
+    std::cout << "Nav X: " << nav_x << "Nav Y: " << nav_y << std::endl;
     
   }
 
