fe8e6e9f5d72a9e7d9854a8e618d2758f11a7791
bradenMeyers <bjz-github@spwiz.com>
2024-11-25 14:29:33 -0700

    Fixed launch scripts to match

--- src/cougars_control/launch/moos_sim_gps_launch.py
+++ src/cougars_control/launch/moos_sim_gps_launch.py
@@ -1,46 +1,39 @@
 import launch
 import launch_ros.actions
 import launch_ros.descriptions
-from launch.substitutions import LaunchConfiguration
-from launch_ros.parameter_descriptions import ParameterFile
-
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
-
-import os
+import sys
 
 def generate_launch_description():
     '''
     :author: Braden Meyers
     :date: September 2024
 
-    Launches the controller and any data convertes needed to test nodes in simulation
+    Launches the controller and any data converters needed to test nodes in simulation
 
     :return: The launch description.
     '''
 
-    config_file = "/home/frostlab/config/sim_config.yaml"
+    for arg in sys.argv:
+        if arg.startswith('namespace:='):
+            namespace = arg.split(':=')[1]
+
+    config_file = "/home/frostlab/config/sim_params.yaml"
 
     return launch.LaunchDescription([
         
         # Define the namespace parameter
-        launch.actions.DeclareLaunchArgument(
-            'namespace',
-            default_value='',
-            description='Unique vehicle namespace'
-        ),
         launch_ros.actions.Node(
             package='cougars_control',
             executable='moos_bridge_sim',
             parameters=[config_file],
-            namespace=LaunchConfiguration('namespace'),
+            namespace=namespace,
             output='screen',
         ),
         launch_ros.actions.Node(
             package='cougars_control',
             executable='coug_controls',
-            parameters=[ParameterFile(config_file, allow_substs=True)],
-            namespace=LaunchConfiguration('namespace'),
+            parameters=[config_file],
+            namespace=namespace,
             output='screen',
         ),
     ])
