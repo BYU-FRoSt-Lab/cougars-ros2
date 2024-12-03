216b18714a89e29670bb9c61eb86b0307da7f71f
ClaytonSmith314 <claytonsmith3.14@gmail.com>
2024-12-03 15:02:31 -0700

    cougars coms sends a stop thruster confirmation message now

--- src/cougars_coms/src/cougars_coms.cpp
+++ src/cougars_coms/src/cougars_coms.cpp
@@ -1,28 +1,35 @@
 
-#include "cougars_coms/coms_protocol.hpp"
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_srvs/srv/set_bool.hpp"
 #include "seatrac_interfaces/msg/modem_rec.hpp"
+#include "seatrac_interfaces/msg/modem_send.hpp"
+
+#include "cougars_coms/coms_protocol.hpp"
 
 #include <iostream>
 #include <chrono>
 #include <memory>
 
 using namespace std::literals::chrono_literals;
-
 using std::placeholders::_1;
 using namespace cougars_coms;
 
 class ComsNode : public rclcpp::Node {
 public:
     ComsNode() : Node("cougars_coms") {
-        this->modem_subscriber = this->create_subscription<seatrac_interfaces::msg::ModemRec>(
+
+        this->declare_parameter<int>("base_station_beacon_id", 15);
+
+        this->base_station_beacon_id_ = this->get_parameter("base_station_beacon_id").as_int();
+
+        this->modem_subscriber_ = this->create_subscription<seatrac_interfaces::msg::ModemRec>(
             "modem_rec", 10,
             std::bind(&ComsNode::listen_to_modem, this, _1)
         );
+        this->modem_publisher_ = this->create_publisher<seatrac_interfaces::msg::ModemSend>("modem_send", 10);
 
-        this->thruster_arming_client = this->create_client<std_srvs::srv::SetBool>(
+        this->thruster_client_ = this->create_client<std_srvs::srv::SetBool>(
             "arm_thruster"
         );
     }
@@ -34,6 +41,9 @@ public:
             case EMPTY: break;
             case EMERGENCY_KILL: {
                 bool success = kill_thruster();
+                ConfirmEmergencyKill response;
+                response.success = success;
+                send_acoustic_message(base_station_beacon_id_, 1, (uint8_t*)&response);
             } break;
         }
     }
@@ -41,7 +51,7 @@ public:
     bool kill_thruster() {
         auto request = std::make_shared<std_srvs::srv::SetBool::Request>();
         request->data = true;
-        while (!this->thruster_arming_client->wait_for_service(1s)) {
+        while (!this->thruster_client_->wait_for_service(1s)) {
             if (!rclcpp::ok()) {
                 RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), 
                     "Interrupted while waiting for the arm_thruster service. Exiting.");
@@ -49,7 +59,7 @@ public:
             }
             RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "arm_thruster service not available, waiting again...");
         }
-        auto result = this->thruster_arming_client->async_send_request(request);
+        auto result = this->thruster_client_->async_send_request(request);
         if (rclcpp::spin_until_future_complete(shared_from_this(), result) ==
             rclcpp::FutureReturnCode::SUCCESS)
         {
@@ -61,11 +71,25 @@ public:
         }
     }
 
+    void send_acoustic_message(int target_id, int message_len, uint8_t* message) {
+        auto request = seatrac_interfaces::msg::ModemSend();
+        request.msg_id = 0x60; //CID_DAT_SEND
+        request.dest_id = (uint8_t)target_id;
+        request.msg_type = 0x0; //MSG_OWAY, data sent one way without response or position data
+        request.packet_len = (uint8_t)std::min(message_len, 31);
+        std::memcpy(&request.packet_data, message, request.packet_len);
+        
+        this->modem_publisher_->publish(request);
+    }
+
 
 private:
 
-    rclcpp::Subscription<seatrac_interfaces::msg::ModemRec>::SharedPtr modem_subscriber;
-    rclcpp::Client<std_srvs::srv::SetBool>::SharedPtr thruster_arming_client;
+    rclcpp::Subscription<seatrac_interfaces::msg::ModemRec>::SharedPtr modem_subscriber_;
+    rclcpp::Publisher<seatrac_interfaces::msg::ModemSend>::SharedPtr modem_publisher_;
+    rclcpp::Client<std_srvs::srv::SetBool>::SharedPtr thruster_client_;
+
+    int base_station_beacon_id_;
 
 };
 
