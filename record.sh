18b6d505c7ade3e4b481b70a8a06ad48316eec62
BradenMeyers <bjz-github@spwiz.com>
2024-12-19 14:47:35 -0700

    Copied over printing statements

--- record.sh
+++ record.sh
@@ -6,6 +6,18 @@
 # - Run this after running the 'launch.sh', 'test.sh' scripts
 # - Log files are saved in 'CoUGARs/bag' on the host machine running the docker container
 
+function printInfo {
+  echo -e "\033[0m\033[36m[INFO] $1\033[0m"
+}
+
+function printWarning {
+  echo -e "\033[0m\033[33m[WARNING] $1\033[0m"
+}
+
+function printError {
+  echo -e "\033[0m\033[31m[ERROR] $1\033[0m"
+}
+
 case $1 in
   "on")
     bash ~/ros2_ws/dvl_tools/acoustics_on.sh true
