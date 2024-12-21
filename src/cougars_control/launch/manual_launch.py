a9022132aa5fb4ecd2d6ae0654fde28b1ed217e4
BradenMeyers <bjz-github@spwiz.com>
2024-12-20 22:27:16 -0700

    flags instead of other options.  converters launch and sensors launch seperated. launch file combined into flags.

--- src/cougars_control/launch/manual_launch.py
+++ src/cougars_control/launch/manual_launch.py
@@ -9,6 +9,7 @@ from ament_index_python.packages import get_package_share_directory
 
 import os
 
+
 def generate_launch_description():
     '''
     :author: Nelson Durrant
@@ -19,23 +20,43 @@ def generate_launch_description():
     :return: The launch description.
     '''
 
+    # Get the directory of the launch files
+    package_dir = os.path.join(
+        get_package_share_directory('cougars_localization'), 'launch')
+
+    sim = "false"  # Default to 'false'
+    verbose = "false"  # Default to 'false'
+    param_file = '/home/frostlab/config/vehicle_params.yaml'
+
     for arg in sys.argv:
         if arg.startswith('namespace:='):
             namespace = arg.split(':=')[1]
         if arg.startswith('param_file:='):
             param_file = arg.split(':=')[1]
+        if arg.startswith("sim:="):
+            sim = arg.split(":=")[1].lower()
+        if arg.startswith("verbose:="):
+            verbose = arg.split(":=")[1].lower()
+    
+    if verbose == "true":
+        output = 'screen'
+    else:
+        output = 'log'
+    
+    launch_actions = []
 
-    # Get the directory of the launch files
-    package_dir = os.path.join(
-        get_package_share_directory('cougars_localization'), 'launch')
-
-    return launch.LaunchDescription([
+    if sim == "false":
+        sensors = launch.actions.IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(package_dir, "sensors_launch.py"))
+        )
+        launch_actions.append(sensors)
+    
+    
+    launch_actions.extend([
         
-        # Include additional launch files
         launch.actions.IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(package_dir, 'sensors_launch.py'))
-        ),
-        # Start the control nodes
+            PythonLaunchDescriptionSource(os.path.join(package_dir, "converters_launch.py"))
+        ),   
         launch_ros.actions.Node(
             package='cougars_control',
             executable='coug_kinematics',
@@ -46,7 +67,7 @@ def generate_launch_description():
             package='cougars_control',
             executable='coug_controls',
             parameters=[param_file],
-            output='log',
+            output=output,
             namespace=namespace,
         ),
         launch_ros.actions.Node(
@@ -69,3 +90,5 @@ def generate_launch_description():
         #     namespace=namespace,
         # ),
     ])
+
+    return launch.LaunchDescription(launch_actions)
