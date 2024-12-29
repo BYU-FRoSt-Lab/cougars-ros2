4f48eb3cb4c4f4d77f3da42468af8d3e59cb68bd
Braden Meyers <bjz-github@spwiz.com>
2024-12-28 17:56:02 -0700

    WIP: Surface service created. Needs to check the sign of the values.

--- src/cougars_control/src/coug_kinematics.cpp
+++ src/cougars_control/src/coug_kinematics.cpp
@@ -89,6 +89,17 @@ public:
             std::bind(&CougKinematics::arm_thruster, this, std::placeholders::_1, std::placeholders::_2));
 
     /**
+     * @brief Kinematics pitch up override service.
+     *
+     * This service recieves requests on the "/surface" service.
+     * It uses the std_srvs/srv/SetBool service type.
+     * False will allow for normal control, true will override any commands and pitch vehicle up
+     */
+    surface_service_ = this->create_service<std_srvs::srv::SetBool>(
+            "surface", 
+            std::bind(&CougKinematics::surface, this, std::placeholders::_1, std::placeholders::_2));
+
+    /**
      * @brief Kinematics command publisher.
      *
      * This publisher publishes the commands to the "kinematics/command" topic.
@@ -125,15 +136,22 @@ private:
 
     auto command = frost_interfaces::msg::UCommand();
     command.header.stamp = msg.header.stamp;
-    command.fin[0] =
-        msg.fin[0] + this->get_parameter("top_fin_offset").as_double() +
-        this->get_parameter("trim_ratio").as_double() * msg.thruster;
-    command.fin[1] = -1 *
-        msg.fin[1] + this->get_parameter("right_fin_offset").as_double() +
-        this->get_parameter("trim_ratio").as_double() * msg.thruster;
-    command.fin[2] =
-        msg.fin[2] + this->get_parameter("left_fin_offset").as_double() +
-        this->get_parameter("trim_ratio").as_double() * msg.thruster;
+    
+    if(!surface_override_){
+      command.fin[0] =
+          msg.fin[0] + this->get_parameter("top_fin_offset").as_double() +
+          this->get_parameter("trim_ratio").as_double() * msg.thruster;
+      command.fin[1] = -1 *
+          msg.fin[1] + this->get_parameter("right_fin_offset").as_double() +
+          this->get_parameter("trim_ratio").as_double() * msg.thruster;
+      command.fin[2] =
+          msg.fin[2] + this->get_parameter("left_fin_offset").as_double() +
+          this->get_parameter("trim_ratio").as_double() * msg.thruster;
+    } else {
+      command.fin[0] = 0;
+      command.fin[1] = -25;  // TODO: make sure this pitches the vehicle up!!
+      command.fin[2] = 25;
+    }
 
     if (this->get_parameter("demo_mode").as_bool()) {
       command.thruster = 0;
@@ -156,7 +174,27 @@ private:
     RCLCPP_INFO(this->get_logger(), response->message.c_str());
   }
 
+  void surface(const std::shared_ptr<std_srvs::srv::SetBool::Request> request,
+                        std::shared_ptr<std_srvs::srv::SetBool::Response> response)
+  {
+    // Set the boolean to the requested value
+    surface_override_ = request->data;
+    response->success = true;
+    response->message = surface_override_ ? "Fin Override!" : "Fin actuation!";
+    RCLCPP_INFO(this->get_logger(), response->message.c_str());
+
+    //Publish the fins up command
+    auto command = frost_interfaces::msg::UCommand();
+
+    command.fin[0] = 0;
+    command.fin[1] = -25;  // TODO: make sure this pitches the vehicle up!!
+    command.fin[2] = 25;
+
+    command_publisher_->publish(command);    
+  }
+
   bool arm_thruster_ = false;
+  bool surface_override_ = false;
 
   // ROS objects
   rclcpp::Publisher<frost_interfaces::msg::UCommand>::SharedPtr
