8464fdc20555e95cc90c88c0fd03000287f75b4f
ClaytonSmith314 <claytonsmith3.14@gmail.com>
2025-01-23 17:41:10 -0700

    add coms messages

--- src/cougars_coms/include/cougars_coms/coms_protocol.hpp
+++ src/cougars_coms/include/cougars_coms/coms_protocol.hpp
@@ -10,8 +10,19 @@ namespace cougars_coms {
 
 enum COUG_MSG_ID : uint8_t {
     EMPTY = 0x00,
+
+    VEHICLE_STATUS = 0x10,
+    REQUEST_STATUS = 0x11,
+
+    VERIFY_LAUNCH = 0x20,          
+    CONFIRM_VERIFY = 0x21,
+
+    LAUNCH = 0x30,
+    CONFIRM_LAUNCH = 0x31,
+
     EMERGENCY_KILL = 0xFF,
-    CONFIRM_EMERGENCY_KILL = 0xCF
+    CONFIRM_EMERGENCY_KILL = 0xFC,
+
 };
 
 struct EmergencyKill {
