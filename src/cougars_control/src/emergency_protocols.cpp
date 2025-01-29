8e7c4f6ed334017fff67c8cfb504991aa77f6461
Matthew McMurray <mmcmurray123@gmail.com>
2025-01-28 17:13:11 -0700

    initial add of emergency handler node

--- src/cougars_control/src/emergency_protocols.cpp
+++ src/cougars_control/src/emergency_protocols.cpp
@@ -1,79 +1,128 @@
-#include <chrono>
-#include <functional>
 #include <memory>
-#include <string>
-#include "frost_interfaces/msg/leak_status.hpp"
+
 #include "rclcpp/rclcpp.hpp"
 
-
-using namespace std::chrono_literals;
-using std::placeholders::_1;
-
-rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
-auto qos = rclcpp::QoS(
-    rclcpp::QoSInitialization(qos_profile.history, qos_profile.depth),
-    qos_profile);
-
-/**
- * @brief A node for handling control for errors in vehicle system
- * @author Matthew McMurray
- * @date Jan 2025
- *
- */
-class EmergencyProtocols : public rclcpp::Node {
+class EmergencyParameterHandler : public rclcpp::Node
+{
 public:
+  EmergencyParameterHandler()
+  : Node("emergency_parameter_handler")
+  {
+     // problems that could be detected
+    this->declare_parameter("leak_detected", false);
+    this->declare_parameter("low_battery_detected", false);
+    this->declare_parameter("collision_detected", false);
+    this->declare_parameter("moos_error_detected", false);
+    this->declare_parameter("factor_graph_error_detected", false);
+    this->declare_parameter("controls_node_error_detected", false);
+    this->declare_parameter("no_heartbeat_error_detected", false);
+    this->declare_parameter("no_progress_waypoint_error_detected", false);
+    this->declare_parameter("too_deep_detected", false);
+    this->declare_parameter("mission_timeout_detected", false);
+    this->declare_parameter("unable_to_dive_detected", false);
+
+    // Create a parameter subscriber that can be used to monitor parameter changes
+    // (for this node's parameters as well as other nodes' parameters)
+    param_subscriber_ = std::make_shared<rclcpp::ParameterEventHandler>(this);
+
+    auto handle_problem_callback = [this](const rclcpp::Parameter & p) {
+
+        // display emergency 
+
+        if (p.as_bool() == true){
+
+          std::string parameter = p.get_name().c_str();
+
+          RCLCPP_INFO(
+            this->get_logger(), "\nPROBLEM DETECTED: Parameter \"%s\" = \"%s\"",
+            p.get_name().c_str(),
+            p.as_bool() ? "true" : "false");
 
 
-  /**
-   * Creates a new emergency protocols node.
-   */
-  EmergencyProtocols() : Node("emergency_protocols") {
+          if (parameter ==  "collision_detected"){
+            // TODO: Implement service to deal with emergency
+          }
+          else if (parameter == "controls_node_error_detected"){
+            // TODO: Implement service to deal with emergency
+          }
+          else if (parameter == "factor_graph_error_detected"){
+            // TODO: Implement service to deal with emergency
+          }
+          else if (parameter == "leak_detected"){
+            // TODO: Implement service to deal with emergency
+          }
+          else if (parameter == "low_battery_detected"){
+            // TODO: Implement service to deal with emergency
+          }
+          else if (parameter == "mission_timeout_detected"){
+            // TODO: Implement service to deal with emergency
+          }
+          else if (parameter == "moos_error_detected"){
+            // TODO: Implement service to deal with emergency
+          }
+          else if (parameter == "no_heartbeat_error_detected"){
+            // TODO: Implement service to deal with emergency
+          }
+          else if (parameter == "no_progress_waypoint_error_detected"){
+            // TODO: Implement service to deal with emergency
+          }
+          else if (parameter == "too_deep_detected"){
+            // TODO: Implement service to deal with emergency
+          }
+          else if (parameter == "unable_to_dive_detected"){
+            // TODO: Implement service to deal with emergency
+          }
+        } 
+      };
 
 
-
-    // Emergencies below
-
-
-    // leak
-    leak_status_subscription_ =
-        this->create_subscription<frost_interfaces::msg::LeakStatus>(
-            "leak/data", 10,
-            std::bind(&EmergencyProtocols::leak_callback, this, _1));
+    // collision_detected
+    // controls_node_error_detected
+    // factor_graph_error_detected
+    // leak_detected
+    // low_battery_detected
+    // mission_timeout_detected
+    // moos_error_detected
+    // no_heartbeat_error_detected
+    // no_progress_waypoint_error_detected
+    // too_deep_detected
+    // unable_to_dive_detected
+    collision_detected_handle_ = param_subscriber_->add_parameter_callback("collision_detected", handle_problem_callback);
+    controls_node_error_detected_handle_ = param_subscriber_->add_parameter_callback("controls_node_error_detected", handle_problem_callback);
+    factor_graph_error_detected_handle_ = param_subscriber_->add_parameter_callback("factor_graph_error_detected", handle_problem_callback);
+    leak_detected_handle_ = param_subscriber_->add_parameter_callback("leak_detected", handle_problem_callback);
+    low_battery_detected_handle_ = param_subscriber_->add_parameter_callback("low_battery_detected", handle_problem_callback);
+    mission_timeout_detected_handle_ = param_subscriber_->add_parameter_callback("mission_timeout_detected", handle_problem_callback);
+    moos_error_detected_handle_ = param_subscriber_->add_parameter_callback("moos_error_detected", handle_problem_callback);
+    no_heartbeat_error_detected_handle = param_subscriber_->add_parameter_callback("no_heartbeat_error_detected", handle_problem_callback);
+    no_progress_waypoint_error_detected_handle_ = param_subscriber_->add_parameter_callback("no_progress_waypoint_error_detected", handle_problem_callback);
+    too_deep_detected_handle_ = param_subscriber_->add_parameter_callback("too_deep_detected", handle_problem_callback);
+    unable_to_dive_detected_handle_ = param_subscriber_->add_parameter_callback("unable_to_dive_detected", handle_problem_callback);
 
   }
 
-private:
+ private:
+  std::shared_ptr<rclcpp::ParameterEventHandler> param_subscriber_;
+  std::shared_ptr<rclcpp::ParameterCallbackHandle> collision_detected_handle_;
+  std::shared_ptr<rclcpp::ParameterCallbackHandle> controls_node_error_detected_handle_;
+  std::shared_ptr<rclcpp::ParameterCallbackHandle> leak_detected_handle_;
+  std::shared_ptr<rclcpp::ParameterCallbackHandle> factor_graph_error_detected_handle_;
+  std::shared_ptr<rclcpp::ParameterCallbackHandle> low_battery_detected_handle_;
+  std::shared_ptr<rclcpp::ParameterCallbackHandle> mission_timeout_detected_handle_;
+  std::shared_ptr<rclcpp::ParameterCallbackHandle> moos_error_detected_handle_;
+  std::shared_ptr<rclcpp::ParameterCallbackHandle> no_heartbeat_error_detected_handle;
+  std::shared_ptr<rclcpp::ParameterCallbackHandle> no_progress_waypoint_error_detected_handle_;
+  std::shared_ptr<rclcpp::ParameterCallbackHandle> too_deep_detected_handle_;
+  std::shared_ptr<rclcpp::ParameterCallbackHandle> unable_to_dive_detected_handle_;
+ 
 
-
-
-//
-
- void
-  leak_callback(const frost_interfaces::msg::LeakStatus &leak_msg) {
-
-
-    
-  }
-
-
-
-
-
-
-// add subscribers here
-
-
-
-
-
-  rclcpp::Subscription<frost_interfaces::msg::LeakStatus>::SharedPtr
-      leak_status_subscription_;
-  
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char ** argv)
+{
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<EmergencyProtocols>());
+  rclcpp::spin(std::make_shared<EmergencyParameterHandler>());
   rclcpp::shutdown();
+
   return 0;
 }
