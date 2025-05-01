b13466531edda09c3922b5c366727af04390cff7
BradenMeyers <bjz-github@spwiz.com>
2025-04-30 22:27:31 -0600

    bag recorder updates and depth convertor calibration on startup

--- src/cougars_localization/launch/sensors_launch.py
+++ src/cougars_localization/launch/sensors_launch.py
@@ -61,6 +61,12 @@ def generate_launch_description():
         #     executable='micro_ros_agent',
         #     arguments=['serial', '--dev', '/dev/ttyACM0', '-b', '6000000'],
         # ),
+        launch_ros.actions.Node(
+            package='cougars_control',
+            executable='emergency_protocols',
+            parameters=[param_file, fleet_param],
+            namespace=namespace,
+        ),
 
         # Serial Teensy connection
         launch_ros.actions.Node(
