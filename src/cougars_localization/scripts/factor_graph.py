02bd3571c6adaa5fd11ddb919b1e0d93d04ae38d
Eli Gaskin <egaskin2@byu.edu>
2025-05-23 13:01:43 -0600

    changes before rebasing for odom covariance add

--- src/cougars_localization/scripts/factor_graph.py
+++ src/cougars_localization/scripts/factor_graph.py
@@ -47,10 +47,10 @@ class FactorGraphNode(Node):
         self.prev_x = 0
 
         # number of seconds we take a dvl dead reck. pose/factor graph callback time
-        self.declare_parameter("factor_callback_seconds",1.25) #was 2s
+        self.declare_parameter("factor_callback_seconds",.75) #was 2s
         self.dvl_time_interval =  self.get_parameter("factor_callback_seconds").get_parameter_value().integer_value 
 
-        self.declare_parameter("max_factor_graph_size",40) #max number of factors
+        self.declare_parameter("max_factor_graph_size",50) #max number of factors
 
         # flags to start the whole system
         self.depth_received = False
