3b5a262ea0ad04e55b093d724e80710024fb5c93
BradenMeyers <bjz-github@spwiz.com>
2025-06-30 12:04:01 -0600

    added second pressure sensor

--- src/pressure_sensor/pressure_sensor/pressure_pub.py
+++ src/pressure_sensor/pressure_sensor/pressure_pub.py
@@ -5,7 +5,7 @@ from sensor_msgs.msg import FluidPressure
 from . import ms5837
 
 class PressurePublisher(Node):
-    def __init__(self, sensor_model, node_name):
+    def __init__(self, sensor_model, node_name, bus, frame_id):
         super().__init__(node_name)
         self.declare_parameter('sensor_type', sensor_model)
         sensor_type = self.get_parameter('sensor_type').get_parameter_value().integer_value
@@ -13,10 +13,10 @@ class PressurePublisher(Node):
         self.publisher_ = self.create_publisher(FluidPressure, 'pressure/data', 10)
 
         if sensor_type == 0:
-            self.sensor = ms5837.MS5837_02BA()
+            self.sensor = ms5837.MS5837_02BA(bus=bus)
             self.get_logger().info("Using MS5837_02BA sensor (shallow)")
         else:
-            self.sensor = ms5837.MS5837_30BA()
+            self.sensor = ms5837.MS5837_30BA(bus=bus)
             self.get_logger().info("Using MS5837_30BA sensor (deep)")
 
         if not self.sensor.init():
@@ -24,12 +24,14 @@ class PressurePublisher(Node):
             return
 
         self.timer = self.create_timer(0.1, self.timer_callback)  # 10 Hz
+        self.frame_id = frame_id
 
     def timer_callback(self):
         if self.sensor.read():
             pressure_pa = self.sensor.pressure() * 100  # mbar to Pascal
             msg = FluidPressure()
             msg.header.stamp = self.get_clock().now().to_msg()
+            msg.header.frame_id = self.frame_id
             msg.fluid_pressure = pressure_pa
             msg.variance = 0.0
 
@@ -44,8 +46,8 @@ def main():
     node = None
     node2 = None
     try:
-        node = PressurePublisher(0, 'pressure_publisher_02ba')
-        node2 = PressurePublisher(1, 'pressure_publisher_30ba')
+        node = PressurePublisher(0, 'pressure_publisher_02ba', 1, "shallow_pressure")
+        node2 = PressurePublisher(1, 'pressure_publisher_30ba', 0, "deep_pressure")
         if node is None or node2 is None:
             rclpy.shutdown()
             return
