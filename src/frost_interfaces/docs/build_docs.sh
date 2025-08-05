7e18ad716d6e66bc92c30614307df6f324d793c4
Braden <bjz-github@spwiz.com>
2025-08-05 01:17:13 -0600

    Changed frost interfaces to cougars interfaces

--- src/frost_interfaces/docs/build_docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Created by Nelson Durrant, Oct 2024
-#
-# Generates documentation for the ROS2 frost_interfaces package
-# - The documentation can be accessed by opening the 'index.html'
-#   file in the 'docs_output/frost_interfaces' directory
-
-source ~/ros2_ws/install/setup.bash
-rosdoc2 build --package-path ~/ros2_ws/src/frost_interfaces
