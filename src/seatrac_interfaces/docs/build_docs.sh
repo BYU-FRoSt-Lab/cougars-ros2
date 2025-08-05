fce72c0236c3c2eaf9bda78f3a3cf0f9ebf1b064
Braden <bjz-github@spwiz.com>
2025-08-05 10:54:21 -0600

    removed seatrac packages to there own repository

--- src/seatrac_interfaces/docs/build_docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Created by Nelson Durrant, Oct 2024
-#
-# Generates documentation for the ROS2 seatrac_interfaces package
-# - The documentation can be accessed by opening the 'index.html'
-#   file in the 'docs_output/seatrac_interfaces' directory
-
-source ~/ros2_ws/install/setup.bash
-rosdoc2 build --package-path ~/ros2_ws/src/seatrac_interfaces
