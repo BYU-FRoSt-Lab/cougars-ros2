284957210065ec97f5a78ae12fd63de3163200f5
Ben Washburn <benrobertwashburn@gmail.com>
2025-08-12 16:23:56 -0600

    fix dependency more

--- src/cougars_coms/rf_bridge/rf_bridge.py
+++ src/cougars_coms/rf_bridge/rf_bridge.py
@@ -9,7 +9,7 @@ from sensor_msgs.msg import BatteryState, FluidPressure
 from geometry_msgs.msg import TwistWithCovarianceStamped, PoseWithCovarianceStamped
 from dvl_msgs.msg import DVLDR
 from std_srvs.srv import SetBool
-from frost_interfaces import SystemStatus
+from frost_interfaces.msg import SystemStatus
 
 from digi.xbee.devices import XBeeDevice, RemoteXBeeDevice, XBee64BitAddress
 from digi.xbee.exception import TransmitException
