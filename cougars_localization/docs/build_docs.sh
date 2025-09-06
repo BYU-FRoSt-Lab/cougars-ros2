21022152b0098722b04cff83cadc0a4695e5b4da
Braden Meyers <bjz-github@spwiz.com>
2025-09-05 22:03:28 -0600

    Removed old docs files

--- cougars_localization/docs/build_docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Created by Nelson Durrant, Oct 2024
-#
-# Generates documentation for the ROS2 cougars_localization package
-# - The documentation can be accessed by opening the 'index.html'
-#   file in the 'docs_output/cougars_localization' directory
-
-source ~/ros2_ws/install/setup.bash
-rosdoc2 build --package-path ~/ros2_ws/src/cougars_localization
